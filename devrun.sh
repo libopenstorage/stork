a2a986b8a35fa22af9a94ba984a961e169a4cbbb
Gou Rao <gou@portworx.com>
2017-05-31 05:23:15 +0000

    inline volume spec test

--- devrun.sh
+++ devrun.sh
@@ -1,6 +1,5 @@
 # This sets up the env vars to test torpedo directly as a binary.
 
-export DOCKER_HOST="unix:///var/run/docker.sock"
-export CLUSTER_NODES="172.31.18.146,72.31.19.148,172.31.5.96"
+export CLUSTER_NODES="172.31.18.146,172.31.19.148,172.31.5.96"
 
 ./torpedo docker pxd
