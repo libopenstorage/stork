c896d9dcd56f6456f972e25015713c4b6c821685
Gou Rao <gou@portworx.com>
2017-09-07 18:45:28 +0000

    added a vendor dir

--- cmd/stork/stork.go
+++ cmd/stork/stork.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/libopenstorage/stork/drivers"
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage %v <driver name>", os.Args[0])
+		os.Exit(-1)
+	}
+
+	drv := os.Args[1]
+
+	d, err := drivers.Get(drv)
+	if err != nil {
+		log.Fatalf("Error getting Stork Driver %v: %v", drv, err)
+		os.Exit(-1)
+	}
+
+	if err = d.Init(); err != nil {
+		log.Fatalf("Error initializing Stork Driver %v: %v", drv, err)
+		os.Exit(-1)
+	}
+
+	os.Exit(0)
 }
