45b02e70c29af8f258fbee96050453468c65940f
Harsh Desai <harsh@portworx.com>
2017-10-16 20:49:58 -0700

    Add script to run torpedo aws run on jenkins

---
+++ deployments/deploy-aws.sh
@@ -0,0 +1,155 @@
+#!/bin/bash -x
+
+EXTRA_ARGS=""
+
+if [ -n "${VERBOSE}" ]; then
+    VERBOSE="--v"
+fi
+
+
+if [ -n "${SKIP_TESTS}" ]; then
+    EXTRA_ARGS="--skip=$SKIP_TESTS $EXTRA_ARGS"
+fi
+
+
+if [ -z "${TORPEDO_IMG}" ]; then
+    TORPEDO_IMG="portworx/torpedo:latest"
+    echo "Using default torpedo image: ${TORPEDO_IMG}"
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "[ERROR] AWS_ACCESS_KEY_ID environment variable not set."
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "[ERROR] AWS_SECRET_ACCESS_KEY environment variable not set."
+    exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+    echo "[ERROR] AWS_REGION environment variable not set."
+    exit 1
+fi
+
+kubectl delete secret aws-access || true
+kubectl delete pod torpedo || true
+
+echo "Creating AWS secret..."
+kubectl create secret generic aws-access \
+        --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        --from-literal=AWS_REGION=$AWS_REGION
+
+
+echo "Deploying torpedo pod..."
+cat <<EOF | kubectl create -f -
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: torpedo-account
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+metadata:
+   name: torpedo-role
+rules:
+  -
+    apiGroups:
+      # have access to everything except Secrets
+      - "*"
+    resources: ["*"]
+    verbs: ["*"]
+  - nonResourceURLs: ["*"]
+    verbs: ["*"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+metadata:
+  name: torpedo-role-binding
+subjects:
+- apiVersion: v1
+  kind: ServiceAccount
+  name: torpedo-account
+  namespace: default
+roleRef:
+  kind: ClusterRole
+  name: torpedo-role
+  apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: torpedo
+spec:
+  tolerations:
+  - key: node-role.kubernetes.io/master
+    operator: Equal
+    effect: NoSchedule
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: node-role.kubernetes.io/master
+            operator: Exists
+  containers:
+  - name: torpedo
+    image: ${TORPEDO_IMG}
+    command: [ "ginkgo" ]
+    args: [ "$VERBOSE",
+            "--trace",
+            "--failFast",
+            "$EXTRA_ARGS",
+             "--slowSpecThreshold", "360",
+            "bin/basic.test",
+            "bin/reboot.test",
+            "--",
+            "--spec-dir", "../drivers/scheduler/k8s/specs",
+            "--node-driver", "aws" ]
+    tty: true
+    env:
+      - name: AWS_ACCESS_KEY_ID
+        valueFrom:
+          secretKeyRef:
+            name: aws-access
+            key: AWS_ACCESS_KEY_ID
+      - name: AWS_SECRET_ACCESS_KEY
+        valueFrom:
+          secretKeyRef:
+            name: aws-access
+            key: AWS_SECRET_ACCESS_KEY
+      - name: AWS_REGION
+        valueFrom:
+          secretKeyRef:
+            name: aws-access
+            key: AWS_REGION
+  restartPolicy: Never
+  serviceAccountName: torpedo-account
+EOF
+
+echo "Waiting for torpedo to start running"
+
+for i in $(seq 1 300) ; do
+  printf .
+  state=`kubectl get pod torpedo | grep -v NAME | awk '{print $3}'`
+  if [ "$state" == "Running" ] || [ "$state" == "Completed" ]; then
+    echo ""
+    kubectl logs -f torpedo
+
+    endState=`kubectl get pod torpedo | grep -v NAME | awk '{print $3}'`
+    if [ "$endState" == "Running" ] || [ "$endState" == "Completed" ]; then
+        exit 0
+    else
+        echo "Error: Torpedo finished with $endState state"
+        exit 1
+    fi
+  fi
+
+  sleep 1
+done
+
+echo "Failed to wait for torpedo to start running..."
+kubectl describe pod torpedo
+exit 1
---
+++ deployments/deploy-ssh.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -x
+
+EXTRA_ARGS=""
+
+if [ -n "${VERBOSE}" ]; then
+    VERBOSE="--v"
+fi
+
+if [ -n "${SKIP_TESTS}" ]; then
+    EXTRA_ARGS="--skip=$SKIP_TESTS $EXTRA_ARGS"
+fi
+
+if [ -z "${TORPEDO_IMG}" ]; then
+    TORPEDO_IMG="portworx/torpedo:latest"
+    echo "Using default torpedo image: ${TORPEDO_IMG}"
+fi
+
+kubectl delete pod torpedo || true
+
+echo "Deploying torpedo pod..."
+cat <<EOF | kubectl create -f -
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: torpedo-account
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+metadata:
+   name: torpedo-role
+rules:
+  -
+    apiGroups:
+      # have access to everything except Secrets
+      - "*"
+    resources: ["*"]
+    verbs: ["*"]
+  - nonResourceURLs: ["*"]
+    verbs: ["*"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1alpha1
+metadata:
+  name: torpedo-role-binding
+subjects:
+- apiVersion: v1
+  kind: ServiceAccount
+  name: torpedo-account
+  namespace: default
+roleRef:
+  kind: ClusterRole
+  name: torpedo-role
+  apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: torpedo
+spec:
+  tolerations:
+  - key: node-role.kubernetes.io/master
+    operator: Equal
+    effect: NoSchedule
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: node-role.kubernetes.io/master
+            operator: Exists
+  containers:
+  - name: torpedo
+    image: ${TORPEDO_IMG}
+    command: [ "ginkgo" ]
+    args: [ "$VERBOSE",
+            "--trace",
+            "--failFast",
+            "$EXTRA_ARGS",
+             "--slowSpecThreshold", "360",
+            "bin/basic.test",
+            "bin/reboot.test",
+            "--",
+            "--spec-dir", "../drivers/scheduler/k8s/specs",
+            "--node-driver", "ssh" ]
+    tty: true
+  restartPolicy: Never
+  serviceAccountName: torpedo-account
+EOF
+
+echo "Waiting for torpedo to start running"
+
+for i in $(seq 1 300) ; do
+  printf .
+  state=`kubectl get pod torpedo | grep -v NAME | awk '{print $3}'`
+  if [ "$state" == "Running" ] || [ "$state" == "Completed" ]; then
+    echo ""
+    kubectl logs -f torpedo
+    sleep 5
+    endState=`kubectl get pod torpedo | grep -v NAME | awk '{print $3}'`
+    if [ "$endState" == "Running" ] || [ "$endState" == "Completed" ]; then
+        echo "Success: Torpedo finished with $endState state"
+        exit 0
+    else
+        echo "Error: Torpedo finished with $endState state"
+        exit 1
+    fi
+  fi
+
+  sleep 1
+done
+
+echo "Failed to wait for torpedo to start running..."
+kubectl describe pod torpedo
+exit 1
