30a60f425ef830e07ee505219528c47f92b8020a
Harsh Desai <harsh@portworx.com>
2017-10-21 16:47:59 -0700

    fix deploy aws script

--- deployments/deploy-aws.sh
+++ deployments/deploy-aws.sh
@@ -138,8 +138,11 @@ for i in $(seq 1 300) ; do
     echo ""
     kubectl logs -f torpedo
 
+    sleep 5
+
     endState=`kubectl get pod torpedo | grep -v NAME | awk '{print $3}'`
     if [ "$endState" == "Running" ] || [ "$endState" == "Completed" ]; then
+        echo "Success: Torpedo finished with $endState state"
         exit 0
     else
         echo "Error: Torpedo finished with $endState state"
