cf84d3bd5c4d02b539b9c1de6d423aed2d290cd9
Piyush Nimbalkar <piyush@portworx.com>
2017-11-17 11:03:31 -0800

    Using v1beta2 for app objects - Changing the specs as parse and manage only one version of given object for now.

--- drivers/scheduler/k8s/k8s.go
+++ drivers/scheduler/k8s/k8s.go
@@ -18,7 +18,7 @@ import (
 	"github.com/portworx/torpedo/drivers/scheduler/spec"
 	"github.com/portworx/torpedo/drivers/volume"
 	"github.com/sirupsen/logrus"
-	apps_api "k8s.io/api/apps/v1beta1"
+	apps_api "k8s.io/api/apps/v1beta2"
 	"k8s.io/api/core/v1"
 	storage_api "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/types"
