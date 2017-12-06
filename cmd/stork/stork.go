5ca3a853f435c9ed4e4cad5a6056ce02275355f6
Dinesh Israni <disrani@portworx.com>
2017-12-05 17:24:31 -0800

    Update version to 0.2

--- cmd/stork/stork.go
+++ cmd/stork/stork.go
@@ -17,7 +17,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "stork"
 	app.Usage = "STorage Orchestartor Runtime for Kubernetes (STORK)"
-	app.Version = "0.1"
+	app.Version = "0.2"
 	app.Action = run
 
 	app.Flags = []cli.Flag{
