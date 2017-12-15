e12beeed9f155e00952c8d8372f50f3ee5bad58e
Dinesh Israni <disrani@portworx.com>
2017-12-14 18:38:41 -0800

    Add support for leader election when starting up
    Leader election is enabled by default
    Can specify lock object name and namespace as cmd args
    
    Issue #10

--- cmd/stork/stork.go
+++ cmd/stork/stork.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/golang/glog"
 	"github.com/libopenstorage/stork/drivers/volume"
 	_ "github.com/libopenstorage/stork/drivers/volume/portworx"
 	"github.com/libopenstorage/stork/pkg/extender"
@@ -13,6 +14,15 @@ import (
 	"github.com/libopenstorage/stork/pkg/snapshot"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
+	api_v1 "k8s.io/api/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	core_v1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/leaderelectionconfig"
 )
 
 func main() {
@@ -42,6 +52,20 @@ func main() {
 			Name:  "driver,d",
 			Usage: "Storage driver name",
 		},
+		cli.BoolTFlag{
+			Name:  "leader-elect",
+			Usage: "Enable leader election",
+		},
+		cli.StringFlag{
+			Name:  "lock-object-name",
+			Usage: "Name for the lock object (default: stork)",
+			Value: "stork",
+		},
+		cli.StringFlag{
+			Name:  "lock-object-namespace",
+			Usage: "Namespace for the lock object (default: kube-system)",
+			Value: "kube-system",
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -49,6 +73,7 @@ func main() {
 	}
 }
 func run(c *cli.Context) {
+
 	driverName := c.String("driver")
 	if len(driverName) == 0 {
 		log.Fatalf("driver option is required")
@@ -60,6 +85,83 @@ func run(c *cli.Context) {
 		log.SetLevel(log.DebugLevel)
 	}
 
+	runFunc := func(_ <-chan struct{}) {
+		runStork(driverName)
+	}
+
+	leaderConfig := leaderelectionconfig.DefaultLeaderElectionConfiguration()
+	leaderConfig.LeaderElect = c.BoolT("leader-elect")
+
+	if leaderConfig.LeaderElect {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalf("Error getting cluster config: %v", err)
+			os.Exit(-1)
+		}
+
+		k8sClient, err := clientset.NewForConfig(config)
+		if err != nil {
+			log.Fatalf("Error getting client, %v", err)
+			os.Exit(-1)
+		}
+
+		leaderConfig.ResourceLock = resourcelock.ConfigMapsResourceLock
+		lockObjectName := c.String("lock-object-name")
+		lockObjectNamespace := c.String("lock-object-namespace")
+
+		eventBroadcaster := record.NewBroadcaster()
+		eventBroadcaster.StartLogging(glog.Infof)
+		eventBroadcaster.StartRecordingToSink(&core_v1.EventSinkImpl{Interface: core_v1.New(k8sClient.Core().RESTClient()).Events("")})
+		recorder := eventBroadcaster.NewRecorder(api.Scheme, api_v1.EventSource{Component: "stork"})
+
+		id, err := os.Hostname()
+		if err != nil {
+			log.Fatalf("Error getting hostname: %v", err)
+			os.Exit(-1)
+		}
+
+		lockConfig := resourcelock.ResourceLockConfig{
+			Identity:      id,
+			EventRecorder: recorder,
+		}
+
+		resourceLock, err := resourcelock.New(
+			leaderConfig.ResourceLock,
+			lockObjectNamespace,
+			lockObjectName,
+			k8sClient.CoreV1(),
+			lockConfig)
+		if err != nil {
+			log.Fatalf("Error creating resource lock: %v", err)
+			os.Exit(-1)
+		}
+
+		leaderElector, err := leaderelection.NewLeaderElector(
+			leaderelection.LeaderElectionConfig{
+				Lock:          resourceLock,
+				LeaseDuration: leaderConfig.LeaseDuration.Duration,
+				RenewDeadline: leaderConfig.RenewDeadline.Duration,
+				RetryPeriod:   leaderConfig.RetryPeriod.Duration,
+
+				Callbacks: leaderelection.LeaderCallbacks{
+					OnStartedLeading: runFunc,
+					OnStoppedLeading: func() {
+						log.Fatalf("Stork lost master")
+					},
+				},
+			})
+		if err != nil {
+			log.Fatalf("Error creating leader elector: %v", err)
+			os.Exit(-1)
+		}
+
+		leaderElector.Run()
+	} else {
+		runFunc(nil)
+	}
+}
+
+func runStork(driverName string) {
 	d, err := volume.Get(driverName)
 	if err != nil {
 		log.Fatalf("Error getting Stork Driver %v: %v", driverName, err)
