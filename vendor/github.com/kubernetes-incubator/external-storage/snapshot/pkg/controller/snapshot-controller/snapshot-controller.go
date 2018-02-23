c60ddf5412e0c7e3d33e8428fd8206a750bfc292
Dinesh Israni <disrani@portworx.com>
2018-02-23 14:52:51 -0800

    Update sync cache for snapshot controller
    Issue #29

--- vendor/github.com/kubernetes-incubator/external-storage/snapshot/pkg/controller/snapshot-controller/snapshot-controller.go
+++ vendor/github.com/kubernetes-incubator/external-storage/snapshot/pkg/controller/snapshot-controller/snapshot-controller.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/client-go/tools/record"
 
 	"k8s.io/kubernetes/pkg/api"
+	k8scontroller "k8s.io/kubernetes/pkg/controller"
 
 	crdv1 "github.com/kubernetes-incubator/external-storage/snapshot/pkg/apis/crd/v1"
 	"github.com/kubernetes-incubator/external-storage/snapshot/pkg/controller/cache"
@@ -168,16 +169,11 @@ func (c *snapshotController) Run(ctx <-chan struct{}) {
 	glog.Infof("Starting snapshot controller")
 
 	go c.snapshotController.Run(ctx)
-	for n := 0; n < 10; n++ {
-		if c.snapshotController.HasSynced() {
-			break
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
-	if !c.snapshotController.HasSynced() {
-		glog.Errorf("Error syncing the controller")
+
+	if !k8scontroller.WaitForCacheSync("snapshot-controller", ctx, c.snapshotController.HasSynced) {
 		return
 	}
+
 	go c.reconciler.Run(ctx)
 	go c.desiredStateOfWorldPopulator.Run(ctx)
 
