749c9beb5a63b208e3c35f1f46f9f5d79ce5d2ab
Dinesh Israni <disrani@portworx.com>
2018-02-28 13:37:18 -0800

    Update test script to use master image tag

--- test/integration_test/test-deploy.sh
+++ test/integration_test/test-deploy.sh
@@ -5,6 +5,7 @@ apk add jq
 
 KUBEVERSION=$(kubectl version -o json | jq ".serverVersion.gitVersion" -r)
 sed -i 's/<kube_version>/'"$KUBEVERSION"'/g' /specs/stork-scheduler.yaml
+sed -i 's/'stork:.*'/'stork:master'/g' specs/stork-deployment.yaml
 
 echo "Creating stork deployment"
 kubectl delete -f /specs/stork-deployment.yaml
