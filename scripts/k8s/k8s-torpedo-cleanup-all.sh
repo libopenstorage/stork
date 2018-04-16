62cebdb1daf5b76c53004341d52a92e8ed1f838a
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2018-04-16 16:22:06 -0700

    Do not need delete all the storage classes in cleanup (#142)

--- scripts/k8s/k8s-torpedo-cleanup-all.sh
+++ scripts/k8s/k8s-torpedo-cleanup-all.sh
@@ -4,6 +4,3 @@ for ns in `kubectl get ns -l creater=torpedo | grep Active | awk '{print $1}'`; 
     echo "Cleaning up torpedo from namespace: ${ns}"
     kubectl delete namespace "${ns}"
 done
-
-kubectl delete sc --all
-
