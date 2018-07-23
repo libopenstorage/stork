4b9beb34380476521b8dae6b8013f200381bd9f3
Dinesh Israni <disrani@portworx.com>
2018-07-23 15:27:07 -0700

    Bump version to 1.1.3

--- cmd/stork/stork.go
+++ cmd/stork/stork.go
@@ -43,7 +43,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "stork"
 	app.Usage = "STorage Orchestartor Runtime for Kubernetes (STORK)"
-	app.Version = "1.1.2"
+	app.Version = "1.1.3"
 	app.Action = run
 
 	app.Flags = []cli.Flag{
