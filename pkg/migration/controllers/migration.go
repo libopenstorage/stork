57de4b753abfddbf933b8f5f8f8e070493bfc9d1
Dinesh Israni <disrani@portworx.com>
2018-09-24 13:49:12 -0700

    Update resync parameter for controller for time.Duraion

--- pkg/migration/controllers/migration.go
+++ pkg/migration/controllers/migration.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/heptio/ark/pkg/discovery"
 	"github.com/heptio/ark/pkg/util/collections"
@@ -34,7 +35,7 @@ import (
 )
 
 const (
-	resyncPeriod                     = 30
+	resyncPeriod                     = 30 * time.Second
 	storkMigrationReplicasAnnotation = "stork.openstorage.org/migrationReplicas"
 )
 
