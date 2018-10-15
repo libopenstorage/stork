8c7f87a20f35798bc3aafb153e5433979cbfd8bb
Craig Rodrigues <craig@portworx.com>
2018-10-15 16:21:11 -0700

    Add test for snapshots
    - Add test functions for mock testing kubernetes API server
      These functions are taken from 'get_test.go' in the kubernetes
      repository.

---
+++ pkg/storkctl/factory_test.go
@@ -0,0 +1,19 @@
+// +build unittest
+
+package storkctl
+
+import (
+	cmdtesting "k8s.io/kubernetes/pkg/kubectl/cmd/testing"
+)
+
+type TestFactory struct {
+	cmdtesting.TestFactory
+	Factory
+}
+
+func NewTestFactory() *TestFactory {
+	return &TestFactory{
+		TestFactory: *cmdtesting.NewTestFactory(),
+		Factory:     NewFactory(),
+	}
+}
