260794bc09c75797facefca1a80278de3ef7ff26
Dinesh Israni <disrani@portworx.com>
2018-11-28 19:04:29 -0800

    Update storkctl UTs to use base command

--- pkg/storkctl/pvc_test.go
+++ pkg/storkctl/pvc_test.go
@@ -7,36 +7,36 @@ import (
 )
 
 func TestCreatePVCNoName(t *testing.T) {
-	cmdArgs := []string{"pvc"}
+	cmdArgs := []string{"create", "pvc"}
 
 	expected := "error: Exactly one argument needs to be provided for pvc name"
-	testCommon(t, newCreateCommand, cmdArgs, nil, expected, true)
+	testCommon(t, cmdArgs, nil, expected, true)
 }
 
 func TestCreatePVCNoSnap(t *testing.T) {
-	cmdArgs := []string{"pvc", "pvc1"}
+	cmdArgs := []string{"create", "pvc", "pvc1"}
 
 	expected := "error: Snapshot name needs to be given"
-	testCommon(t, newCreateCommand, cmdArgs, nil, expected, true)
+	testCommon(t, cmdArgs, nil, expected, true)
 }
 
 func TestCreatePVCNoSize(t *testing.T) {
-	cmdArgs := []string{"pvc", "-s", "snap1", "pvc1"}
+	cmdArgs := []string{"create", "pvc", "-s", "snap1", "pvc1"}
 
 	expected := "error: Size needs to be provided"
-	testCommon(t, newCreateCommand, cmdArgs, nil, expected, true)
+	testCommon(t, cmdArgs, nil, expected, true)
 }
 
 func TestCreatePVC(t *testing.T) {
-	cmdArgs := []string{"pvc", "--snapshot", "snap1", "--size", "1", "pvc1"}
+	cmdArgs := []string{"create", "pvc", "--snapshot", "snap1", "--size", "1", "pvc1"}
 
 	expected := "PersistentVolumeClaim pvc1 created successfully\n"
-	testCommon(t, newCreateCommand, cmdArgs, nil, expected, false)
+	testCommon(t, cmdArgs, nil, expected, false)
 }
 
 func TestCreatePVCSourceNamespace(t *testing.T) {
-	cmdArgs := []string{"pvc", "--snapshot", "snap1", "--source-ns", "sourcenamespace", "--size", "1", "pvc2"}
+	cmdArgs := []string{"create", "pvc", "--snapshot", "snap1", "--source-ns", "sourcenamespace", "--size", "1", "pvc2"}
 
 	expected := "PersistentVolumeClaim pvc2 created successfully\n"
-	testCommon(t, newCreateCommand, cmdArgs, nil, expected, false)
+	testCommon(t, cmdArgs, nil, expected, false)
 }
