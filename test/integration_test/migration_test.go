0e0cc0310f577d253d984c64879b450ac710dd0e
Dinesh Israni <disrani@portworx.com>
2019-01-22 19:21:08 -0800

    Add integration tests for migration pre/post exec rules

--- test/integration_test/migration_test.go
+++ test/integration_test/migration_test.go
@@ -10,47 +10,141 @@ import (
 )
 
 func testMigration(t *testing.T) {
-	t.Run("sanityMigrationTest", sanityMigrationTest)
+	t.Run("deploymentTest", deploymentMigrationTest)
+	t.Run("statefulsetTest", statefulsetMigrationTest)
+	t.Run("statefulsetRuleTest", statefulsetMigrationRuleTest)
+	t.Run("preExecRuleMissingTest", statefulsetMigrationRulePreExecMissingTest)
+	t.Run("postExecRuleMissingTest", statefulsetMigrationRulePostExecMissingTest)
+	t.Run("disallowedNamespaceTest", migrationDisallowedNamespaceTest)
+	t.Run("failingPreExecRuleTest", migrationFailingPreExecRuleTest)
+	t.Run("failingPostExecRuleTest", migrationFailingPostExecRuleTest)
 }
 
-func sanityMigrationTest(t *testing.T) {
+func triggerMigrationTest(
+	t *testing.T,
+	instanceID string,
+	appKey string,
+	migrationAppKey string,
+	migrationSuccessExpected bool,
+) {
 	var err error
 	// schedule mysql app on cluster 1
-	mysqlCtxs, err := schedulerDriver.Schedule("singlemysql",
-		scheduler.ScheduleOptions{AppKeys: []string{"mysql-1-pvc"}})
+	ctxs, err := schedulerDriver.Schedule(instanceID,
+		scheduler.ScheduleOptions{AppKeys: []string{appKey}})
 	require.NoError(t, err, "Error scheduling task")
-	require.Equal(t, 1, len(mysqlCtxs), "Only one task should have started")
+	require.Equal(t, 1, len(ctxs), "Only one task should have started")
 
-	err = schedulerDriver.WaitForRunning(mysqlCtxs[0], defaultWaitTimeout, defaultWaitInterval)
+	err = schedulerDriver.WaitForRunning(ctxs[0], defaultWaitTimeout, defaultWaitInterval)
 	require.NoError(t, err, "Error waiting for pod to get to running state")
+
+	preMigrationCtx := ctxs[0].DeepCopy()
 
 	// this is so mysql-1-pvc-namespace is created
 	// create, apply and validate cluster pair specs
-	pairCtxs, err := scheduleClusterPair()
+	err = scheduleClusterPair(ctxs[0])
 	require.NoError(t, err, "Error scheduling cluster pair")
 
 	// apply migration specs
-	migrationCtxs, err := schedulerDriver.Schedule("migration",
-		scheduler.ScheduleOptions{AppKeys: []string{"migration"}})
+	err = schedulerDriver.AddTasks(ctxs[0],
+		scheduler.ScheduleOptions{AppKeys: []string{migrationAppKey}})
 	require.NoError(t, err, "Error scheduling migration specs")
 
-	err = schedulerDriver.WaitForRunning(migrationCtxs[0], defaultWaitTimeout, defaultWaitInterval)
-	require.NoError(t, err, "Error waiting for migration to get to Ready state")
+	err = schedulerDriver.WaitForRunning(ctxs[0], defaultWaitTimeout, defaultWaitInterval)
+	if migrationSuccessExpected {
+		require.NoError(t, err, "Error waiting for migration to get to Ready state")
 
-	// wait on cluster 2 to get mysql pod running
-	err = setRemoteConfig(remoteFilePath)
-	require.NoError(t, err, "Error setting remote config")
-	err = schedulerDriver.WaitForRunning(mysqlCtxs[0], defaultWaitTimeout, defaultWaitInterval)
-	require.NoError(t, err, "Error waiting for pod to get to running state")
-	// destroy mysql app on cluster 2
-	destroyAndWait(t, mysqlCtxs)
+		// wait on cluster 2 to get mysql pod running
+		err = setRemoteConfig(remoteFilePath)
+		require.NoError(t, err, "Error setting remote config")
+		err = schedulerDriver.WaitForRunning(preMigrationCtx, defaultWaitTimeout, defaultWaitInterval)
+		require.NoError(t, err, "Error waiting for pod to get to running state on remote cluster after migration")
+		// destroy mysql app on cluster 2
+		destroyAndWait(t, []*scheduler.Context{preMigrationCtx})
+	} else {
+		require.Error(t, err, "Expected migration to fail")
+	}
 
 	// destroy mysql app on cluster 1
 	err = setRemoteConfig("")
 	require.NoError(t, err, "Error setting remote config")
-	destroyAndWait(t, mysqlCtxs)
+	destroyAndWait(t, ctxs)
+}
 
-	// destroy CRD objects
-	destroyAndWait(t, migrationCtxs)
-	destroyAndWait(t, pairCtxs)
+func deploymentMigrationTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"mysql-migration",
+		"mysql-1-pvc",
+		"mysql-migration",
+		true,
+	)
+}
+
+func statefulsetMigrationTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"cassandra-migration",
+		"cassandra",
+		"cassandra-migration",
+		true,
+	)
+}
+
+func statefulsetMigrationRuleTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"cassandra-migration-rule",
+		"cassandra",
+		"cassandra-migration-rule",
+		true,
+	)
+}
+
+func statefulsetMigrationRulePreExecMissingTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"migration-pre-exec-missing",
+		"mysql-1-pvc",
+		"mysql-migration-pre-exec-missing",
+		false,
+	)
+}
+func statefulsetMigrationRulePostExecMissingTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"migration-post-exec-missing",
+		"mysql-1-pvc",
+		"mysql-migration-post-exec-missing",
+		false,
+	)
+}
+
+func migrationDisallowedNamespaceTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"migration-disallowed-namespace",
+		"mysql-1-pvc",
+		"mysql-migration-disallowed-ns",
+		false,
+	)
+}
+
+func migrationFailingPreExecRuleTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"migration-failing-pre-exec-rule",
+		"mysql-1-pvc",
+		"mysql-migration-failing-pre-exec",
+		false,
+	)
+}
+
+func migrationFailingPostExecRuleTest(t *testing.T) {
+	triggerMigrationTest(
+		t,
+		"migration-failing-post-exec-rule",
+		"mysql-1-pvc",
+		"mysql-migration-failing-post-exec",
+		false,
+	)
 }
