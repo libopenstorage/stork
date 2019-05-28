27dd45ca1fb857578875e5366c069fe4de6e6a8e
Aditya Dani <aditya@portworx.com>
2019-05-27 17:37:59 -0700

    Update dep versions for sched-ops and torpedo packages
    Signed-off-by: Aditya Dani <aditya@portworx.com>

--- vendor/github.com/portworx/sched-ops/task/task.go
+++ vendor/github.com/portworx/sched-ops/task/task.go
@@ -1,39 +1,24 @@
 package task
 
 import (
-	"fmt"
+	"errors"
 	"log"
-	"sync"
 	"time"
 )
 
 //TODO: export the type: type Task func() (string, error)
 
 // ErrTimedOut is returned when an operation times out
-type ErrTimedOut struct {
-	// Reason is the reason for the timeout
-	Reason string
-}
-
-func (e *ErrTimedOut) Error() string {
-	errString := "timed out performing task."
-	if len(e.Reason) > 0 {
-		errString = fmt.Sprintf("%s, Error was: %s", errString, e.Reason)
-	}
-
-	return errString
-}
+var ErrTimedOut = errors.New("timed out performing task")
 
 // DoRetryWithTimeout performs given task with given timeout and timeBeforeRetry
 func DoRetryWithTimeout(t func() (interface{}, bool, error), timeout, timeBeforeRetry time.Duration) (interface{}, error) {
 	done := make(chan bool, 1)
 	quit := make(chan bool, 1)
-	var (
-		out     interface{}
-		err     error
-		errLock sync.Mutex
-		retry   bool
-	)
+	var out interface{}
+	var err error
+	var retry bool
+
 	go func() {
 		count := 0
 		for {
@@ -44,15 +29,11 @@ func DoRetryWithTimeout(t func() (interface{}, bool, error), timeout, timeBefore
 				}
 
 			default:
-				errLock.Lock()
 				out, retry, err = t()
 				if err == nil || !retry {
 					done <- true
-					errLock.Unlock()
 					return
 				}
-
-				errLock.Unlock()
 
 				log.Printf("%v Next retry in: %v", err, timeBeforeRetry)
 				time.Sleep(timeBeforeRetry)
@@ -66,17 +47,7 @@ func DoRetryWithTimeout(t func() (interface{}, bool, error), timeout, timeBefore
 	case <-done:
 		return out, err
 	case <-time.After(timeout):
-		errLock.Lock()
-		defer errLock.Unlock()
 		quit <- true
-
-		var reason string
-		if err != nil {
-			reason = err.Error()
-		}
-
-		return out, &ErrTimedOut{
-			Reason: reason,
-		}
+		return out, ErrTimedOut
 	}
 }
