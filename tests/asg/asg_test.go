a453bfa0b91f4f33af62e0c2eed55b92c8235714
Vinayak Shinde <vinayakshnd@gmail.com>
2019-07-16 20:25:37 +0530

    Addressed review comments

--- tests/asg/asg_test.go
+++ tests/asg/asg_test.go
@@ -2,8 +2,6 @@ package tests
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 	"testing"
 	"time"
 
@@ -21,7 +19,8 @@ import (
 )
 
 const (
-	scaleTimeout = 10 * time.Minute
+	scaleTimeout                = 10 * time.Minute
+	autoNodeRecoveryTimeoutMins = 15 * time.Minute
 )
 
 func TestASG(t *testing.T) {
@@ -41,15 +40,6 @@ var _ = BeforeSuite(func() {
 var _ = Describe("{ClusterScaleUpDown}", func() {
 	It("has to validate that storage nodes are not lost during asg scaledown", func() {
 
-		autoNodeRecoveryTimeout := os.Getenv("ASG_AUTO_NODE_RECOVERY_TIMEOUT_MIN")
-		if autoNodeRecoveryTimeout == "" {
-			// Default storage nodes recovery timeout of 15 minutes
-			autoNodeRecoveryTimeout = "15"
-		}
-
-		autoNodeRecoveryTimeoutMins, err := strconv.Atoi(autoNodeRecoveryTimeout)
-		Expect(err).NotTo(HaveOccurred())
-
 		var contexts []*scheduler.Context
 		for i := 0; i < Inst().ScaleFactor; i++ {
 			contexts = append(contexts, ScheduleAndValidate(fmt.Sprintf("asgscaleupdown-%d", i))...)
@@ -64,7 +54,7 @@ var _ = Describe("{ClusterScaleUpDown}", func() {
 
 			Scale(scaleupCount)
 			Step(fmt.Sprintf("validate number of storage nodes after scale up"), func() {
-				Validate(scaleupCount)
+				ValidateClusterSize(scaleupCount)
 			})
 
 		})
@@ -73,9 +63,9 @@ var _ = Describe("{ClusterScaleUpDown}", func() {
 			intitialNodeCount), func() {
 			Scale(intitialNodeCount)
 
-			Step(fmt.Sprintf("wait for %v minutes for auto recovery of storeage nodes",
-				autoNodeRecoveryTimeout), func() {
-				time.Sleep(time.Duration(autoNodeRecoveryTimeoutMins) * time.Minute)
+			Step(fmt.Sprintf("wait for %s minutes for auto recovery of storeage nodes",
+				autoNodeRecoveryTimeoutMins.String()), func() {
+				time.Sleep(autoNodeRecoveryTimeoutMins)
 			})
 
 			// After scale down, get fresh list of nodes
@@ -87,7 +77,7 @@ var _ = Describe("{ClusterScaleUpDown}", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Step(fmt.Sprintf("validate number of storage nodes after scale down"), func() {
-				Validate(intitialNodeCount)
+				ValidateClusterSize(intitialNodeCount)
 			})
 		})
 
@@ -108,29 +98,29 @@ func init() {
 	ParseFlags()
 }
 
-func Scale(newCount int64) {
+func Scale(count int64) {
 	// In multi-zone ASG cluster, node count is per zone
-	perZoneNewCount := newCount / 3
+	perZoneCount := count / 3
 
-	err := Inst().N.SetASGClusterSize(perZoneNewCount, scaleTimeout)
+	err := Inst().N.SetASGClusterSize(perZoneCount, scaleTimeout)
 	Expect(err).NotTo(HaveOccurred())
 }
 
-func Validate(newCount int64) {
+func ValidateClusterSize(count int64) {
 	// In multi-zone ASG cluster, node count is per zone
-	perZoneNewCount := newCount / 3
+	perZoneCount := count / 3
 
 	// Validate total node count
 	currentNodeCount, err := Inst().N.GetASGClusterSize()
 	Expect(err).NotTo(HaveOccurred())
-	Expect(currentNodeCount).Should(Equal(perZoneNewCount * 3))
+	Expect(currentNodeCount).Should(Equal(perZoneCount * 3))
 
 	// Validate storage node count
 	var expectedStorageNodesPerZone int
-	if Inst().MaxStorageNodesPerAZ <= int(perZoneNewCount) {
+	if Inst().MaxStorageNodesPerAZ <= int(perZoneCount) {
 		expectedStorageNodesPerZone = Inst().MaxStorageNodesPerAZ
 	} else {
-		expectedStorageNodesPerZone = int(perZoneNewCount)
+		expectedStorageNodesPerZone = int(perZoneCount)
 	}
 	storageNodes, err := getStorageNodes()
 	Expect(err).NotTo(HaveOccurred())
