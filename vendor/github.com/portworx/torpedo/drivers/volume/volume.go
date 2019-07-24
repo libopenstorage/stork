0f76402dc6c3e021ab720ff348f8643dcfcebc29
Ram <ram.suradkar@portworx.com>
2019-07-25 00:16:17 +0530

    Add integration test for snapshot restore    - grouplocalsnapshot restore    - cloudsnapshot restore    - addressed review comment
    Signed-off-by: Ram <ram.suradkar@portworx.com>

--- vendor/github.com/portworx/torpedo/drivers/volume/volume.go
+++ vendor/github.com/portworx/torpedo/drivers/volume/volume.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	snap_v1 "github.com/kubernetes-incubator/external-storage/snapshot/pkg/apis/crd/v1"
 	"github.com/libopenstorage/openstorage/api"
 	"github.com/portworx/torpedo/drivers/node"
 	"github.com/portworx/torpedo/pkg/errors"
@@ -70,7 +71,7 @@ type Driver interface {
 	StartDriver(n node.Node) error
 
 	// WaitDriverUpOnNode must wait till the volume driver becomes usable on a given node
-	WaitDriverUpOnNode(n node.Node) error
+	WaitDriverUpOnNode(n node.Node, timeout time.Duration) error
 
 	// WaitDriverDownOnNode must wait till the volume driver becomes unusable on a given node
 	WaitDriverDownOnNode(n node.Node) error
@@ -130,7 +131,7 @@ type Driver interface {
 
 	// ValidateVolumeSnapshotRestore return nil if snapshot is restored successuflly to
 	// given volumes
-	ValidateVolumeSnapshotRestore(vol, snap string, timeStart time.Time) error
+	ValidateVolumeSnapshotRestore(vol string, snapData *snap_v1.VolumeSnapshotData, timeStart time.Time) error
 }
 
 var (
