56eeb702862ce1556600383b3e67fbef48767bab
Ram <ram.suradkar@portworx.com>
2019-07-23 23:06:19 -0700

    Vendor fix for libopenstorage@cloudops
    Signed-off-by: Ram <ram.suradkar@portworx.com>

--- vendor/github.com/libopenstorage/cloudops/gce/gce.go
+++ vendor/github.com/libopenstorage/cloudops/gce/gce.go
@@ -74,6 +74,7 @@ func IsDevMode() bool {
 
 // NewClient creates a new GCE operations client
 func NewClient() (cloudops.Ops, error) {
+
 	var i = new(instance)
 	var err error
 	if metadata.OnGCE() {
@@ -636,8 +637,9 @@ func (s *gceOps) SetInstanceGroupSize(instanceGroupID string,
 	}
 
 	var cluster *container.Cluster
+	var operation *container.Operation
 	if zonalCluster {
-		_, err = s.containerService.Projects.Zones.Clusters.NodePools.SetSize(
+		operation, err = s.containerService.Projects.Zones.Clusters.NodePools.SetSize(
 			s.inst.project,
 			s.inst.clusterLocation,
 			s.inst.clusterName,
@@ -645,13 +647,53 @@ func (s *gceOps) SetInstanceGroupSize(instanceGroupID string,
 			setSizeRequest).Do()
 
 	} else {
-		_, err = s.containerService.Projects.Locations.Clusters.NodePools.SetSize(
+		operation, err = s.containerService.Projects.Locations.Clusters.NodePools.SetSize(
 			nodePoolPath,
 			setSizeRequest).Do()
+
 	}
 
 	if err != nil {
 		return err
+	}
+
+	operationPath := fmt.Sprintf("projects/%s/locations/%s/operations/%s",
+		s.inst.project, s.inst.clusterLocation, operation.Name)
+
+	if timeout > time.Nanosecond {
+		f := func() (interface{}, bool, error) {
+
+			if zonalCluster {
+				operation, err = s.containerService.Projects.Zones.Operations.Get(
+					s.inst.project,
+					s.inst.clusterLocation,
+					operation.Name).Do()
+
+			} else {
+				operation, err = s.containerService.Projects.Locations.Operations.Get(
+					operationPath).Do()
+			}
+
+			if err != nil {
+				// Error occured, just retry
+				return nil, true, err
+			}
+
+			// The operation is done, either cancelled or completed.
+			if operation.Status == "DONE" {
+				return nil, false, nil
+			}
+
+			return nil,
+				true,
+				fmt.Errorf("cluster operation [%v] is in [%s] state. Waiting to become DONE",
+					operation.Name, operation.Status)
+		}
+
+		_, err = task.DoRetryWithTimeout(f, timeout, retrySeconds*time.Second)
+		if err != nil {
+			return err
+		}
 	}
 
 	if timeout > time.Nanosecond {
