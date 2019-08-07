33bb381eebcb31528af18e8d5f4ea7142148a4e4
Dinesh Israni <disrani@portworx.com>
2019-08-06 17:10:40 -0700

    Add integration tests for application backup schedule

--- test/integration_test/common_test.go
+++ test/integration_test/common_test.go
@@ -114,7 +114,7 @@ func TestMain(t *testing.T) {
 		t.Run("CmdExecutor", asyncPodCommandTest)
 		t.Run("Migration", testMigration)
 		t.Run("ApplicationClone", testApplicationClone)
-		t.Run("Backup", testBackup)
+		t.Run("ApplicationBackup", testApplicationBackup)
 		t.Skip("Skipping cluster domain tests")
 	}
 
