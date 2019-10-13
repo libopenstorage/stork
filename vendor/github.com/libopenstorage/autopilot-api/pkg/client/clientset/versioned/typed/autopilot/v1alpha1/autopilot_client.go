dcf012cb3a160ea6b6fae89692803289bc026ed7
Harsh Desai <harsh@portworx.com>
2019-10-13 10:00:07 -0700

    update vendor to use sched-ops kubernetes-1.11
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- vendor/github.com/libopenstorage/autopilot-api/pkg/client/clientset/versioned/typed/autopilot/v1alpha1/autopilot_client.go
+++ vendor/github.com/libopenstorage/autopilot-api/pkg/client/clientset/versioned/typed/autopilot/v1alpha1/autopilot_client.go
@@ -19,8 +19,8 @@ limitations under the License.
 package v1alpha1
 
 import (
-	v1alpha1 "github.com/libopenstorage/autopilot/pkg/apis/autopilot/v1alpha1"
-	"github.com/libopenstorage/autopilot/pkg/client/clientset/versioned/scheme"
+	v1alpha1 "github.com/libopenstorage/autopilot-api/pkg/apis/autopilot/v1alpha1"
+	"github.com/libopenstorage/autopilot-api/pkg/client/clientset/versioned/scheme"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
