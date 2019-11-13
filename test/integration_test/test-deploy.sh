4ac2b39d8e86570b99842ddb1eee7c3205df8dfe
Rohit-PX <rohit@portworx.com>
2019-11-13 00:42:23 -0800

    Scaled integration test for app backup
    Signed-off-by: Rohit-PX <rohit@portworx.com>

--- test/integration_test/test-deploy.sh
+++ test/integration_test/test-deploy.sh
@@ -3,6 +3,7 @@
 initializer="false"
 snapshot_scale=10
 migration_scale=10
+backup_scale=10
 image_name="stork:master"
 test_image_name="stork_test:latest"
 remote_config_path=""
@@ -30,6 +31,12 @@ case $i in
     --migration-scale-count)
         echo "Scale for migration scale test (default 10): $2"
         migration_scale=$2
+        shift
+        shift
+        ;;
+    --backup-scale-count)
+        echo "Scale for migration scale test (default 10): $2"
+        backup_scale=$2
         shift
         shift
         ;;
@@ -194,6 +201,7 @@ fi
 sed -i 's/'storage_provisioner'/'"$storage_provisioner"'/g' /testspecs/stork-test-pod.yaml
 sed -i 's/- -snapshot-scale-count=10/- -snapshot-scale-count='"$snapshot_scale"'/g' /testspecs/stork-test-pod.yaml
 sed -i 's/- -migration-scale-count=10/- -migration-scale-count='"$migration_scale"'/g' /testspecs/stork-test-pod.yaml
+sed -i 's/- -backup-scale-count=10/- -backup-scale-count='"$backup_scale"'/g' /testspecs/stork-test-pod.yaml
 sed -i 's/'username'/'"$SSH_USERNAME"'/g' /testspecs/stork-test-pod.yaml
 sed -i 's/'password'/'"$SSH_PASSWORD"'/g' /testspecs/stork-test-pod.yaml
 sed -i 's/'stork_test:.*'/'"$test_image_name"'/g' /testspecs/stork-test-pod.yaml
