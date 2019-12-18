2c965e14e3e22e2575e36134c918bdbef96f683b
Thiago Gonzaga <thiguetta@users.noreply.github.com>
2019-12-18 19:37:29 -0300

    applying fmt to PTX-2117 (#386)

--- tests/reboot/reboot_test.go
+++ tests/reboot/reboot_test.go
@@ -35,47 +35,47 @@ var _ = Describe("{RebootOneNode}", func() {
 		}
 
 		Step("get nodes for all apps in test and reboot their nodes", func() {
-				nodesToReboot:=node.GetWorkerNodes() 
+			nodesToReboot := node.GetWorkerNodes()
 
 			// Reboot node and check driver status
-				Step(fmt.Sprintf("reboot node one at a time from the node(s): %v",  nodesToReboot), func() {
-					for _, n := range nodesToReboot {
-						err = Inst().N.RebootNode(n, node.RebootNodeOpts{
-							Force: true,
-							ConnectionOpts: node.ConnectionOpts{
-								Timeout:         1 * time.Minute,
-								TimeBeforeRetry: 5 * time.Second,
-							},
+			Step(fmt.Sprintf("reboot node one at a time from the node(s): %v", nodesToReboot), func() {
+				for _, n := range nodesToReboot {
+					err = Inst().N.RebootNode(n, node.RebootNodeOpts{
+						Force: true,
+						ConnectionOpts: node.ConnectionOpts{
+							Timeout:         1 * time.Minute,
+							TimeBeforeRetry: 5 * time.Second,
+						},
+					})
+					Expect(err).NotTo(HaveOccurred())
+
+					Step("wait for node to go down", func() {
+						time.Sleep(20 * time.Second)
+					})
+
+					Step("wait for node to be back up", func() {
+						err = Inst().N.TestConnection(n, node.ConnectionOpts{
+							Timeout:         15 * time.Minute,
+							TimeBeforeRetry: 10 * time.Second,
 						})
 						Expect(err).NotTo(HaveOccurred())
+					})
 
-						Step("wait for node to go down", func() {
-							time.Sleep(20 * time.Second)
-						})
+					Step(fmt.Sprintf("wait to scheduler: %s and volume driver: %s to start",
+						Inst().S.String(), Inst().V.String()), func() {
 
-						Step("wait for node to be back up", func() {
-							err = Inst().N.TestConnection(n, node.ConnectionOpts{
-								Timeout:         15 * time.Minute,
-								TimeBeforeRetry: 10 * time.Second,
-							})
-							Expect(err).NotTo(HaveOccurred())
-						})
+						err = Inst().S.IsNodeReady(n)
+						Expect(err).NotTo(HaveOccurred())
 
-						Step(fmt.Sprintf("wait to scheduler: %s and volume driver: %s to start",
-							Inst().S.String(), Inst().V.String()), func() {
-
-							err = Inst().S.IsNodeReady(n)
-							Expect(err).NotTo(HaveOccurred())
-
-							err = Inst().V.WaitDriverUpOnNode(n, Inst().DriverStartTimeout)
-							if err != nil {
-								diagsErr := Inst().V.CollectDiags(n)
-								Expect(diagsErr).NotTo(HaveOccurred())
-							}
-							Expect(err).NotTo(HaveOccurred())
-						})
-					}
-				})
+						err = Inst().V.WaitDriverUpOnNode(n, Inst().DriverStartTimeout)
+						if err != nil {
+							diagsErr := Inst().V.CollectDiags(n)
+							Expect(diagsErr).NotTo(HaveOccurred())
+						}
+						Expect(err).NotTo(HaveOccurred())
+					})
+				}
+			})
 		})
 
 		ValidateAndDestroy(contexts, nil)
