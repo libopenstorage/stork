2f3710fc8861191abbf492a4bccd9e40dec9f6fa
Rohit-PX <rohit@portworx.com>
2021-08-25 11:42:59 -0700

    New test for volume snapshot restore with vdbench data
    Signed-off-by: Rohit-PX <rohit@portworx.com>

--- test/integration_test/snapshot_restore_test.go
+++ test/integration_test/snapshot_restore_test.go
@@ -3,11 +3,18 @@
 package integrationtest
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	snapv1 "github.com/kubernetes-incubator/external-storage/snapshot/pkg/apis/crd/v1"
+	storkv1 "github.com/libopenstorage/stork/pkg/apis/stork/v1alpha1"
+	k8sextops "github.com/portworx/sched-ops/k8s/externalstorage"
+	storkops "github.com/portworx/sched-ops/k8s/stork"
 	"github.com/portworx/torpedo/drivers/scheduler"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func testSnapshotRestore(t *testing.T) {
@@ -15,6 +22,7 @@ func testSnapshotRestore(t *testing.T) {
 	require.NoError(t, err, "failed to set kubeconfig to source cluster: %v", err)
 
 	t.Run("simpleSnapshotRestoreTest", simpleSnapshotRestoreTest)
+	t.Run("inPlaceSnapshotRestoreDataTest", inPlaceSnapshotRestoreDataTest)
 	if !testing.Short() {
 		t.Run("groupSnapshotRestoreTest", groupSnapshotRestoreTest)
 		t.Run("cloudSnapshotRestoreTest", cloudSnapshotRestoreTest)
@@ -46,7 +54,7 @@ func simpleSnapshotRestoreTest(t *testing.T) {
 		scheduler.ScheduleOptions{AppKeys: []string{"mysql-snap-restore"}})
 	require.NoError(t, err, "Error scheduling task")
 	require.Equal(t, 1, len(snapCtx), "Only one task should have started")
-	verifySnapshot(t, snapCtx, "mysql-data", defaultWaitTimeout)
+	verifySnapshot(t, snapCtx, "mysql-data", 3, 2, true, defaultWaitTimeout)
 	startTime := time.Now()
 	restoreCtx := createInPlaceRestore(t, "splocal", []string{"mysql-snap-inplace-restore"})
 	verifyInPlaceSnapshotRestore(t, restoreCtx, startTime, defaultWaitTimeout)
@@ -119,4 +127,65 @@ func groupCloudSnapshotRestoreTest(t *testing.T) {
 	// cleanup test
 	destroyAndWait(t, snapCtx)
 	destroyAndWait(t, restoreCtx)
+}
+
+func inPlaceSnapshotRestoreDataTest(t *testing.T) {
+	testnamespacePostfix := "snapvdbench"
+	appCtx, err := schedulerDriver.Schedule(testnamespacePostfix,
+		scheduler.ScheduleOptions{AppKeys: []string{"vdbench-repl-2-app"}})
+	require.NoError(t, err, "Error scheduling vdbench app")
+	err = schedulerDriver.WaitForRunning(appCtx[0], defaultWaitTimeout, defaultWaitInterval)
+	require.NoError(t, err, "Error waiting for vdbench app")
+
+	logrus.Infof("Waiting for vdbench to write some data to the PVC")
+	time.Sleep(time.Second * 300)
+
+	// Create snapshot directly using a private method instead of torpedo because
+	// torped creates all specs at once and in this case we want to wait before
+	// creating volumesnapshot as we need data to be written to the PVC first
+
+	snapObj := &snapv1.VolumeSnapshot{
+		Metadata: meta.ObjectMeta{
+			Name:      "vdbench-snapshot",
+			Namespace: fmt.Sprintf("vdbench-repl-2-app-%s", testnamespacePostfix),
+		},
+		Spec: snapv1.VolumeSnapshotSpec{
+			PersistentVolumeClaimName: "vdbench-pvc",
+		},
+	}
+	vdbenchSnap, err := k8sextops.Instance().CreateSnapshot(snapObj)
+	require.NoError(t, err, "Error creating vdbench PVC snapshot")
+
+	err = k8sextops.Instance().ValidateSnapshot(vdbenchSnap.Metadata.Name,
+		vdbenchSnap.Metadata.Namespace,
+		true,
+		defaultWaitTimeout,
+		defaultWaitInterval)
+	require.NoError(t, err, "Failed to validate VolumeSnapshot: %v. Err: %v", vdbenchSnap.Metadata.Name, err)
+
+	logrus.Infof("Writing some more data to the PVC before snapshot restore")
+	time.Sleep(time.Second * 600)
+
+	restoreObj := &storkv1.VolumeSnapshotRestore{
+		Spec: storkv1.VolumeSnapshotRestoreSpec{
+			SourceName:      "vdbench-snapshot",
+			SourceNamespace: fmt.Sprintf("vdbench-repl-2-app-%s", testnamespacePostfix),
+		},
+	}
+	restoreObj.Name = "vdbench-inplace-snapshot-restore"
+	restoreObj.Namespace = fmt.Sprintf("vdbench-repl-2-app-%s", testnamespacePostfix)
+	vdbenchRestore, err := storkops.Instance().CreateVolumeSnapshotRestore(restoreObj)
+	require.NoError(t, err, "Error creating vdbench in-place restore")
+
+	err = storkops.Instance().ValidateVolumeSnapshotRestore(vdbenchRestore.Name, vdbenchRestore.Namespace, 240*time.Second, defaultWaitInterval)
+	require.NoError(t, err, "Error validating vdbench in-place restore")
+
+	// cleanup test
+	destroyAndWait(t, appCtx)
+
+	err = storkops.Instance().DeleteVolumeSnapshotRestore(vdbenchRestore.Name, vdbenchRestore.Namespace)
+	require.NoError(t, err, "Error deleting vdbench in-place restore")
+
+	err = k8sextops.Instance().DeleteSnapshot(snapObj.Metadata.Name, snapObj.Metadata.Namespace)
+	require.NoError(t, err, "Error deleting vdbench PVC snapshot")
 }
