bce771383212b0c6df0f52f8fa33fc5d7133b9be
sivakumar subramani <ssubramani@purestorage.com>
2021-12-03 08:25:53 -0500

    pb-2101: Fixed the issue in isCSISnapshotClassRequired function such that for non CSI volumes, it will directly go the kdmp backup
    	- Added hostaccess scc in the backup job for ocp platform.

--- drivers/volume/kdmp/kdmp.go
+++ drivers/volume/kdmp/kdmp.go
@@ -198,17 +198,7 @@ func isCSISnapshotClassRequired(pvc *v1.PersistentVolumeClaim) bool {
 		// So added check. For other we will try to create CSI snapshot and if it fails, we will take generic backup.
 		return true
 	}
-	// TODO: If storage class is not present, need to make volume inspect call to portworx and check.
-	storageClassName := k8shelper.GetPersistentVolumeClaimClass(pvc)
-	if storageClassName != "" {
-		storageClass, err := storage.Instance().GetStorageClass(storageClassName)
-		if err == nil {
-			if _, ok := storageClass.Parameters[proxyEndpoint]; ok {
-				return false
-			}
-		}
-	}
-	return true
+	return false
 }
 
 func (k *kdmp) StartBackup(backup *storkapi.ApplicationBackup,
