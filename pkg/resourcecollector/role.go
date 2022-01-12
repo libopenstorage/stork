a5bd24191123ef06e5f42769c0f0600cfb15334b
sivakumar subramani <ssubramani@purestorage.com>
2022-01-12 14:29:27 -0500

    pb-2157: retaining the rolebinding subjects if the namespace is not set during restore.

--- pkg/resourcecollector/role.go
+++ pkg/resourcecollector/role.go
@@ -3,6 +3,7 @@ package resourcecollector
 import (
 	"strings"
 
+	"github.com/sirupsen/logrus"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -41,10 +42,24 @@ func (r *ResourceCollector) prepareRoleBindingForApply(
 	if err != nil {
 		return err
 	}
+	subjectWithNs := make([]rbacv1.Subject, 0)
+	subjectWithoutNs := make([]rbacv1.Subject, 0)
+	// Create list of rolebinding subjects that has namspace and pass it to updateSubject to
+	// update destination namespace based on the namespace mapping.
+	for _, subject := range rb.Subjects {
+		if len(subject.Namespace) != 0 {
+			subjectWithNs = append(subjectWithNs, subject)
+		} else {
+			subjectWithoutNs = append(subjectWithoutNs, subject)
+		}
+	}
+	rb.Subjects = subjectWithNs
 	rb.Subjects, err = r.updateSubjects(rb.Subjects, namespaceMappings)
 	if err != nil {
 		return err
 	}
+	rb.Subjects = append(rb.Subjects, subjectWithoutNs...)
+	logrus.Infof("sivakumar --- update rb subjects %+v", rb.Subjects)
 	o, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&rb)
 	if err != nil {
 		return err
