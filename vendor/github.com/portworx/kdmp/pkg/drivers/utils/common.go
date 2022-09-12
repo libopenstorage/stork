01162c944f7b68c7cc0f6a53408f138676b8ba69
Prashanth Kumar <prkumar@purestorage.com>
2022-09-12 04:43:59 +0000

    vendor update for kdmp from nfs-ea for resource export reconciler changes

--- vendor/github.com/portworx/kdmp/pkg/drivers/utils/common.go
+++ vendor/github.com/portworx/kdmp/pkg/drivers/utils/common.go
@@ -110,6 +110,58 @@ func CleanServiceAccount(name, namespace string) error {
 	return nil
 }
 
+// SetupNFSServiceAccount create a service account and bind it to a provided role.
+func SetupNFSServiceAccount(name, namespace string, role *rbacv1.ClusterRole) error {
+	if role != nil {
+		role.Name, role.Namespace = name, namespace
+		role.Annotations = map[string]string{
+			SkipResourceAnnotation: "true",
+		}
+		if _, err := rbacops.Instance().CreateClusterRole(role); err != nil && !errors.IsAlreadyExists(err) {
+			return fmt.Errorf("create %s/%s cluster role: %s", namespace, name, err)
+		}
+		if _, err := rbacops.Instance().CreateClusterRoleBinding(clusterRoleBindingFor(name, namespace)); err != nil && !errors.IsAlreadyExists(err) {
+			return fmt.Errorf("create %s/%s cluster rolebinding: %s", namespace, name, err)
+		}
+	}
+	var sa *corev1.ServiceAccount
+	var err error
+	if sa, err = coreops.Instance().CreateServiceAccount(serviceAccountFor(name, namespace)); err != nil && !errors.IsAlreadyExists(err) {
+		return fmt.Errorf("create %s/%s serviceaccount: %s", namespace, name, err)
+	}
+	t := func() (interface{}, bool, error) {
+		sa, err = coreops.Instance().GetServiceAccount(name, namespace)
+		if err != nil {
+			errMsg := fmt.Sprintf("failed fetching sa [%v/%v]: %v", name, namespace, err)
+			logrus.Tracef("%v", errMsg)
+			return "", true, fmt.Errorf("%v", errMsg)
+		}
+		if sa.Secrets == nil {
+			errMsg := fmt.Sprintf("secret token is missing in sa [%v/%v]", name, namespace)
+			return "", true, fmt.Errorf("%v", errMsg)
+		}
+		return "", false, nil
+	}
+	if _, err := task.DoRetryWithTimeout(t, defaultTimeout, progressCheckInterval); err != nil {
+		errMsg := fmt.Sprintf("max retries done, failed in fetching secret token of sa [%v/%v]: %v ", name, namespace, err)
+		logrus.Errorf("%v", errMsg)
+		// Exhausted all retries
+		return err
+	}
+
+	tokenName := sa.Secrets[0].Name
+	secretToken, err := coreops.Instance().GetSecret(tokenName, namespace)
+	if err != nil {
+		return fmt.Errorf("failed in getting secretToken [%v] of service account [%v/%v]: %v", tokenName, name, namespace, err)
+	}
+	secretToken.Annotations[SkipResourceAnnotation] = "true"
+	_, err = coreops.Instance().UpdateSecret(secretToken)
+	if err != nil {
+		return fmt.Errorf("failed in updating the secretToken [%v] of service account [%v/%v]: %v", tokenName, name, namespace, err)
+	}
+	return nil
+}
+
 func roleBindingFor(name, namespace string) *rbacv1.RoleBinding {
 	return &rbacv1.RoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
@@ -129,6 +181,29 @@ func roleBindingFor(name, namespace string) *rbacv1.RoleBinding {
 		RoleRef: rbacv1.RoleRef{
 			Name:     name,
 			Kind:     "Role",
+			APIGroup: rbacv1.GroupName,
+		},
+	}
+}
+
+func clusterRoleBindingFor(name, namespace string) *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Annotations: map[string]string{
+				SkipResourceAnnotation: "true",
+			},
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      rbacv1.ServiceAccountKind,
+				Name:      name,
+				Namespace: namespace,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			Name:     name,
+			Kind:     "ClusterRole",
 			APIGroup: rbacv1.GroupName,
 		},
 	}
