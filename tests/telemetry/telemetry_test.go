2458a8521ab01e1e26f23d84a8d15a6ff3bdae2a
Jose Rivera <jose@portworx.com>
2022-09-23 07:58:11 +0000

    PWX-27153: Need to make sure to get and use the newly generated profile diags and check s3 for those.
    Signed-off-by: Jose Rivera <jose@portworx.com>

--- tests/telemetry/telemetry_test.go
+++ tests/telemetry/telemetry_test.go
@@ -186,37 +186,73 @@ var _ = Describe("{ProfileOnlyDiags}", func() {
 	})
 	var contexts []*scheduler.Context
 	var diagsFiles []string
+	var existingDiags string
+	var newDiags string
+	var err error
+
 	It("has to collect, validate profile diags on S3", func() {
 		contexts = make([]*scheduler.Context, 0)
 		// One node at a time, collect diags and verify in S3
 		for _, currNode := range node.GetWorkerNodes() {
+			// Get the most recent profile diags for comparison
+			Step(fmt.Sprintf("Check latest profile diags on node %v", currNode.Name), func() {
+				logrus.Infof(" Getting latest profile  diags on %v", currNode.Name)
+				existingDiags, err = runCmd(fmt.Sprintf("ls -t /var/cores/%s-*.{stack,heap}.gz | head -n 2", currNode.Name),
+					currNode, nil)
+				if err == nil {
+					logrus.Infof("Found latest profiles diags on node %s:\n%s ", currNode.Name, existingDiags)
+				} else {
+					existingDiags = ""
+				}
+			})
+			// Issue a profile diags to generate new files.
 			Step(fmt.Sprintf("collect profile diags on node: %s | %s", currNode.Name, currNode.Type), func() {
-
 				config := &torpedovolume.DiagRequestConfig{
 					DockerHost: "unix:///var/run/docker.sock",
 					Profile:    true,
 				}
-				if !TelemetryEnabled(currNode) {
+				if !TelemetryEnabled(currNode) { // Not sure why this is done?
 					logrus.Debugf("Telemetry not enabled, sleeping for 5 mins")
 					time.Sleep(5 * time.Minute)
 				}
 				err := Inst().V.CollectDiags(currNode, config, torpedovolume.DiagOps{Validate: true})
+				if err != nil {
+					logrus.Errorf("Failed to collect diags: %v", err)
+				}
 				Expect(err).NotTo(HaveOccurred(), "Profile only diags collected successfully")
 			})
-			Step(fmt.Sprintf("Get the profile diags collected above %s", currNode.Name), func() {
+			// Get the latest files in the directory.  The newly generated files will not equal the most recent. So you know they are new.
+			Step(fmt.Sprintf("Get the new profile diags on node %v", currNode.Name), func() {
 				logrus.Infof("Getting latest profile diags on %66v", currNode.Name)
-				out, err := runCmd(fmt.Sprintf("ls -t /var/cores/%s-*.{stack,heap}.gz | head -n 2", currNode.Name), currNode, nil)
-				if err != nil {
-					logrus.Fatalf("Error in getting profile diags files on: %s, err: %v", currNode.Name, err)
-				}
-				diagsFiles = strings.Split(out, "\n")
+				Eventually(func() bool {
+					newDiags, err = runCmd(fmt.Sprintf("ls -t /var/cores/%s-*.{stack,heap}.gz | head -n 2", currNode.Name),
+						currNode, nil)
+					if err == nil {
+						if existingDiags != "" && existingDiags == newDiags {
+							logrus.Infof("No new profile diags found...")
+							newDiags = ""
+						}
+						if len(newDiags) > 0 {
+							logrus.Infof("Found new profile diags:\n%s", newDiags)
+							// Needs to contain both stack/heap
+							if strings.Contains(newDiags, ".heap") && strings.Contains(newDiags, ".stack") {
+								diagsFiles = strings.Split(newDiags, "\n")
+								return true
+							}
+						}
+					}
+					return false
+				}, 5*time.Minute, 15*time.Second).Should(BeTrue(), "failed to generate profile diags on node %s", currNode.Name)
 			})
 			Step(fmt.Sprintf("Validate diags uploaded on S3"), func() {
 				for _, file := range diagsFiles {
 					fileNameToCheck := path.Base(file)
 					logrus.Debugf("Validating file %s", fileNameToCheck)
-					if TelemetryEnabled(currNode) {
+					if TelemetryEnabled(currNode) { // This is done in case the system is run without telemetry.
 						err := Inst().V.ValidateDiagsOnS3(currNode, fileNameToCheck)
+						if err != nil {
+							logrus.Errorf("Failed to validate diags: %v", err)
+						}
 						Expect(err).NotTo(HaveOccurred(), "Files validated on s3")
 					} else {
 						logrus.Debugf("Telemetry not enabled on %s, skipping test", currNode.Name)
