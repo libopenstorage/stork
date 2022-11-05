bfa2104b504836692ec2568825edeb5af0f37008
sivakumar subraani <sivakumar subramani>
2022-11-05 13:56:09 +0000

    pb-3100: Added check to take kdmp backup, if it is PX volume and NFS backuplocation.

--- drivers/volume/volume.go
+++ drivers/volume/volume.go
@@ -120,7 +120,7 @@ type Driver interface {
 
 	// OwnsPVCForBackup returns true if the PVC is owned by the driver
 	// Since we have extra check need to done for backup case, added seperate version of API.
-	OwnsPVCForBackup(coreOps core.Ops, pvc *v1.PersistentVolumeClaim, cmBackupType string, crBackupType string) bool
+	OwnsPVCForBackup(coreOps core.Ops, pvc *v1.PersistentVolumeClaim, cmBackupType string, crBackupType string, blType storkapi.BackupLocationType) bool
 
 	// OwnsPV returns true if the PV is owned by the driver
 	OwnsPV(pvc *v1.PersistentVolume) bool
@@ -342,13 +342,14 @@ func GetPVCDriverForBackup(coreOps core.Ops,
 	pvc *v1.PersistentVolumeClaim,
 	cmBackupType string,
 	crBackupType string,
+	blType storkapi.BackupLocationType,
 ) (string, error) {
 	for _, driverName := range orderedListOfDrivers {
 		d, ok := volDrivers[driverName]
 		if !ok {
 			continue
 		}
-		if d.OwnsPVCForBackup(coreOps, pvc, cmBackupType, crBackupType) {
+		if d.OwnsPVCForBackup(coreOps, pvc, cmBackupType, crBackupType, blType) {
 			return driverName, nil
 		}
 	}
