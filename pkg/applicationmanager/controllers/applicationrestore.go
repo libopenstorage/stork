b8e8bb15a71f363048a8621cb0e10cedfdf8bc17
Prashanth Kumar <prkumar@purestorage.com>
2022-11-17 01:47:16 +0000

    pb-3306: Removing setting of CR status to success before checking for          all resources status

--- pkg/applicationmanager/controllers/applicationrestore.go
+++ pkg/applicationmanager/controllers/applicationrestore.go
@@ -1569,10 +1569,6 @@ func (a *ApplicationRestoreController) restoreResources(
 						resource.Status,
 						resource.Reason)
 				}
-				restore.Status.Stage = storkapi.ApplicationRestoreStageFinal
-				restore.Status.FinishTimestamp = metav1.Now()
-				restore.Status.Status = storkapi.ApplicationRestoreStatusSuccessful
-				restore.Status.Reason = "Volumes and resources were restored up successfully"
 			case kdmpapi.ResourceExportStatusInitial:
 				doCleanup = false
 			case kdmpapi.ResourceExportStatusPending:
