4a65c4f488c4751d58402557bd334edf843498e4
Kesavan Thiruvenkadasamy <kthiruvenkadasamy@purestorage.com>
2023-02-22 10:08:37 +0530

    Handled nil pointer case when rancher client is not created
    Signed-off-by: Kesavan Thiruvenkadasamy <kthiruvenkadasamy@purestorage.com>

--- pkg/applicationmanager/controllers/applicationbackup.go
+++ pkg/applicationmanager/controllers/applicationbackup.go
@@ -1048,7 +1048,9 @@ func updateProjectDisplayNames(
 	platformCredential *stork_api.PlatformCredential,
 ) error {
 	rancherClient := &rancher.Rancher{}
-	rancherClient.Init(*platformCredential.Spec.RancherConfig)
+	if err := rancherClient.Init(*platformCredential.Spec.RancherConfig); err != nil {
+		return err
+	}
 	projectList, err := rancherClient.ListProjectNames()
 	if err != nil {
 		return err
