49e03fcb17c1e10d5fcfff55b7296459c680f36a
Divyam Goel <dgoel@portworx.com>
2023-02-23 20:34:04 +0000

    Renamed old CreateCRD -> CreateCRDV1 and added a new CreateCRD

--- pkg/applicationmanager/controllers/applicationbackupschedule.go
+++ pkg/applicationmanager/controllers/applicationbackupschedule.go
@@ -521,7 +521,7 @@ func (s *ApplicationBackupScheduleController) createCRD() error {
 		return err
 	}
 	if ok {
-		err := k8sutils.CreateCRD(resource)
+		err := k8sutils.CreateCRDV1(resource)
 		if err != nil && !errors.IsAlreadyExists(err) {
 			return err
 		}
