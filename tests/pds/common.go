a34b19dcb9e903697c51ef92d4de4053923557c4
dbhatnagar-px <117347853+dbhatnagar-px@users.noreply.github.com>
2023-03-24 17:20:03 +0530

    Resiliency suite for PDS and first Test Case (#1199)
    * first draft of resiliency suite
    
    * adding resiliency_suite.go file
    
    * test-1
    
    * testing fix of instantiating Inst in resiliency utils
    
    * Changing Failure type definition
    
    * Cleaning up code for review
    
    * Making a switch case to select resiliency failure
    
    * Removing unintended marking of Scale test as resiliency
    
    * Incorporating Review Comments
    
    * Testing a fix
    
    * testing a fix
    
    * testing : getnodebyname function
    
    * testing a fix for race condition where opd is created but is not assigned a node
    
    * fixing a bug where channel was having redundant data with no reciever to listen to
    
    * Removing unwanted comments for final review
    
    * Making check till replica value a parameter to be passed from Jenkins params
    
    * fixing a minor bug
    
    * Update common.go
    
    * Incorporating Leelas comments
    
    * Removing run only for one data service for final merge
    
    * Small fix of making function generic for resiliency and non-resiliency runs

--- tests/pds/common.go
+++ tests/pds/common.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	pds "github.com/portworx/pds-api-go-client/pds/v1alpha1"
+
 	"github.com/portworx/sched-ops/k8s/core"
 	pdslib "github.com/portworx/torpedo/drivers/pds/lib"
 	"github.com/portworx/torpedo/pkg/aetosutil"
@@ -23,24 +24,25 @@ type PDSDataService struct {
 }
 
 const (
-	pdsNamespace            = "pds-system"
-	deploymentName          = "qa"
-	envDeployAllDataService = "DEPLOY_ALL_DATASERVICE"
-	postgresql              = "PostgreSQL"
-	cassandra               = "Cassandra"
-	elasticSearch           = "Elasticsearch"
-	couchbase               = "Couchbase"
-	redis                   = "Redis"
-	rabbitmq                = "RabbitMQ"
-	mongodb                 = "MongoDB"
-	mysql                   = "MySQL"
-	kafka                   = "Kafka"
-	zookeeper               = "ZooKeeper"
-	consul                  = "Consul"
-	pdsNamespaceLabel       = "pds.portworx.com/available"
-	timeOut                 = 30 * time.Minute
-	maxtimeInterval         = 30 * time.Second
-	timeInterval            = 1 * time.Second
+	pdsNamespace                     = "pds-system"
+	deploymentName                   = "qa"
+	envDeployAllDataService          = "DEPLOY_ALL_DATASERVICE"
+	postgresql                       = "PostgreSQL"
+	cassandra                        = "Cassandra"
+	elasticSearch                    = "Elasticsearch"
+	couchbase                        = "Couchbase"
+	redis                            = "Redis"
+	rabbitmq                         = "RabbitMQ"
+	mongodb                          = "MongoDB"
+	mysql                            = "MySQL"
+	kafka                            = "Kafka"
+	zookeeper                        = "ZooKeeper"
+	consul                           = "Consul"
+	pdsNamespaceLabel                = "pds.portworx.com/available"
+	timeOut                          = 30 * time.Minute
+	maxtimeInterval                  = 30 * time.Second
+	timeInterval                     = 1 * time.Second
+	ActiveNodeRebootDuringDeployment = "active-node-reboot-during-deployment"
 )
 
 var (
