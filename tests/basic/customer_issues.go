56a39c29cb94e08988b9a7595ef0581b2113169d
vprabhakar-px <120549478+vprabhakar-px@users.noreply.github.com>
2023-04-11 19:03:34 +0530

    Update to test function

--- tests/basic/customer_issues.go
+++ tests/basic/customer_issues.go
@@ -277,7 +277,7 @@ var _ = Describe("{FordRunFlatResync}", func() {
 			Inst().AppList = append(Inst().AppList, eachApp)
 		}
 		for i := 0; i < Inst().GlobalScaleFactor; i++ {
-			contexts = append(contexts, ScheduleApplications(fmt.Sprintf("runflatresync-%d", i))...)
+			contexts = append(contexts, ScheduleApplications(fmt.Sprintf("replresyncpoolexpand-%d", i))...)
 		}
 
 		time.Sleep(2 * time.Minute)
@@ -371,6 +371,14 @@ var _ = Describe("{FordRunFlatResync}", func() {
 		}
 		fmt.Println(volumeState)
 
+		revertZone1 := func() {
+			log.FailOnError(blockIptableRules(zone1, zone2, true), "Failed to unblock IPTable rules on target Nodes")
+		}
+
+		revertZone2 := func() {
+			log.FailOnError(blockIptableRules(zone2, zone1, true), "Failed to unblock IPTable rules on target Nodes")
+		}
+
 		// From Zone 1 block all the traffic to systems under zone2
 		// From Zone 2 block all the traffic to systems under zone1
 		log.InfoD("blocking iptables from all nodes present in zone1 from accessing zone2")
@@ -384,15 +392,12 @@ var _ = Describe("{FordRunFlatResync}", func() {
 		log.FailOnError(err, "Failed to set IPtable Rules on zone2")
 
 		// Reverting back Zone1 iptables set
-		for _, each := range zone1 {
-			log.FailOnError(flushIptableRules(each), "failed to flush iptable rules on node from zone1 [%v]", each.Name)
-		}
+		revertZone1()
 
-		for _, each := range zone2 {
-			log.FailOnError(flushIptableRules(each), "failed to flush iptable rules on node from zone2 [%v]", each.Name)
-		}
+		// Reverting back zone2 iptables set
+		revertZone2()
 
-		// set iptable rules on vms under zone2 slowly one after other to restrict connection to the vm's under zone1
+		// Reset iptables rules on vms under zone2
 		err = blockIptableRules(zone2, zone1, false)
 		log.FailOnError(err, "Failed to set IPtable Rules on zone2")
 
@@ -421,7 +426,6 @@ var _ = Describe("{FordRunFlatResync}", func() {
 	JustAfterEach(func() {
 		defer EndTorpedoTest()
 		for _, eachNode := range node.GetNodes() {
-			log.InfoD("flushing all iptable rules on node [%v]", eachNode.Name)
 			log.FailOnError(flushIptableRules(eachNode), "Iptables flush all failed on node [%v]", eachNode.Name)
 		}
 		AfterEachTest(contexts)
