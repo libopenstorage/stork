ebb4c4ddfdf1661ba89e8c1d0dbea0694bf479df
vprabhakar-px <120549478+vprabhakar-px@users.noreply.github.com>
2023-07-27 13:03:52 +0530

    fix for vsphere cloud drive ingore

--- drivers/node/vsphere/vsphere.go
+++ drivers/node/vsphere/vsphere.go
@@ -6,6 +6,7 @@ import (
 	"github.com/vmware/govmomi/vim25/mo"
 	"net/url"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/portworx/sched-ops/task"
@@ -179,8 +180,16 @@ func (v *vsphere) connect() error {
 		var vmMo mo.VirtualMachine
 		err = vm.Properties(v.ctx, vm.Reference(), []string{"guest"}, &vmMo)
 		if err != nil {
-			log.Errorf("failed to get properties: %v", err)
-			return err
+			re, regErr := regexp.Compile(".*has already been deleted or has not been completely created.*")
+			if regErr != nil {
+				return regErr
+			}
+			if re.MatchString(fmt.Sprintf("%v", err)) {
+				log.Info(err)
+			} else {
+				log.Errorf("failed to get properties: %v", err)
+				return err
+			}
 		}
 
 		// Get the hostname
