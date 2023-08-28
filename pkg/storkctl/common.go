e0a065476527af96c1f1732e954db50414bea209
diptiranjan <dipti@portworx.com>
2023-08-28 14:30:13 +0530

    PWX-31372: storkctl to support backing up specific resource types and restoring specific objects.

--- pkg/storkctl/common.go
+++ pkg/storkctl/common.go
@@ -3,7 +3,13 @@ package storkctl
 import (
 	"fmt"
 	"io"
+	"strings"
 	"time"
+
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	discovery "k8s.io/client-go/discovery"
+	"k8s.io/utils/strings/slices"
 )
 
 func toTimeString(t time.Time) string {
@@ -22,4 +28,30 @@ func printMsg(msg string, out io.Writer) {
 	if _, printErr := fmt.Fprintln(out, msg); printErr != nil {
 		fmt.Println(msg)
 	}
+}
+
+func getDiscoveryClientForApiResources() (discovery.DiscoveryInterface, error) {
+	tempFactory := NewFactory()
+	config, err := tempFactory.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+	aeclient, err := apiextensionsclient.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("error getting apiextension client, %v", err)
+	}
+
+	return aeclient.Discovery(), nil
+}
+
+func isValidResourceType(resourceType string, apiResource metav1.APIResource) bool {
+	resourceType = strings.ToLower(resourceType)
+	// comparing all small cases
+	if resourceType == strings.ToLower(apiResource.Name) ||
+		resourceType == strings.ToLower(apiResource.Kind) ||
+		resourceType == strings.ToLower(apiResource.SingularName) ||
+		slices.Contains(apiResource.ShortNames, resourceType) {
+		return true
+	}
+	return false
 }
