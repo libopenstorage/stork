fb7f734c53a8ec8cf0d94142a05ace7719b9e14c
Neelesh Thakur <neelesh.thakur@purestorage.com>
2023-09-07 20:43:54 -0700

    PWX-33267: update px-statfs configmap if needed (#1493)
    When a new virt-launcher pod is starting, update the existing px-statfs configMap, if needed.
    
    Signed-off-by: Neelesh Thakur <neelesh.thakur@purestorage.com>

--- test/integration_test/px_statfs_webhook_test.go
+++ test/integration_test/px_statfs_webhook_test.go
@@ -4,9 +4,11 @@
 package integrationtest
 
 import (
+	"bytes"
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/portworx/sched-ops/k8s/core"
 	"github.com/portworx/torpedo/drivers/node"
@@ -14,6 +16,13 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	statfsConfigMapName = "px-statfs"
+	cmBinaryDataFileKey = "px_statfs.so"
+	cmDataPreloadKey    = "ld.so.preload"
+	cmDataSumKey        = "px_statfs.so.sha256"
 )
 
 // This test is named starting with "TestExtender" so that is runs as part of the TestExtender suite
@@ -54,9 +63,16 @@ func TestExtenderWebhookStatfs(t *testing.T) {
 		require.NoError(t, err, "Error waiting for app to get to running state")
 	}
 
+	foundVirtLauncher := false
 	for _, appCtx := range ctxs {
 		validateStatfs(t, appCtx)
+
+		if !foundVirtLauncher && isVirtLauncherContext(appCtx) {
+			foundVirtLauncher = true
+			validateConfigMapUpdate(t, appCtx)
+		}
 	}
+	require.True(t, foundVirtLauncher)
 
 	logrus.Infof("Destroying apps")
 	destroyAndWait(t, ctxs)
@@ -152,4 +168,127 @@ func runCommandInSv4TestContainer(t *testing.T, pod *corev1.Pod, cmd []string) s
 
 func isVirtLauncherContext(ctx *scheduler.Context) bool {
 	return strings.HasPrefix(ctx.App.Key, "virt-launcher-sim")
+}
+
+// This test verifies that the webhook updates the px-statfs configMap, as neeed.
+// Test steps:
+//
+// - Start with a "good" px-statfs configmap that the webhook has already created.
+// - Make some dummy changes to the px-statfs configmap
+// - Trigger the webhook by deleting one of the virt-launcher-sim pod (new pod should start)
+// - Wait until the webhook has restored the original configMap
+func validateConfigMapUpdate(t *testing.T, ctx *scheduler.Context) {
+	require.True(t, isVirtLauncherContext(ctx))
+	vols, err := schedulerDriver.GetVolumes(ctx)
+	require.NoError(t, err, "Failed to get volumes for context %s", ctx.App.Key)
+
+	require.Equal(t, len(vols), 1)
+	vol := vols[0]
+
+	testCases := []func(cm *corev1.ConfigMap){
+		func(cm *corev1.ConfigMap) {
+			// TestCase: value of "ld.so.preload" key in data is missing px-statfs.so
+			// - change the contents of ld.so.preload file in the configmap
+			cm.Data[cmDataPreloadKey] = "dummy-value"
+		},
+		func(cm *corev1.ConfigMap) {
+			// TestCase: checksum mismatch
+			cm.Data[cmDataSumKey] = "dummy-value"
+			cm.BinaryData[cmBinaryDataFileKey] = updateBytesForTesting(cm.BinaryData[cmBinaryDataFileKey])
+		},
+		func(cm *corev1.ConfigMap) {
+			// TestCase: no checksum
+			delete(cm.Data, cmDataSumKey)
+			cm.BinaryData[cmBinaryDataFileKey] = updateBytesForTesting(cm.BinaryData[cmBinaryDataFileKey])
+		},
+	}
+	for i, updateFn := range testCases {
+		logrus.Infof("Testing configmap update function %d", i)
+		pods, err := core.Instance().GetPodsUsingPV(vol.ID)
+		require.NoError(t, err, "Failed to get pods for volume %v of context %s", vol.ID, ctx.App.Key)
+
+		require.True(t, len(pods) > 0)
+
+		// pick a pod that is not terminating already
+		var pod corev1.Pod
+		for _, pod = range pods {
+			if pod.DeletionTimestamp.IsZero() {
+				logrus.Infof("Using pod %s/%s for the configMap update test", pod.Namespace, pod.Name)
+				break
+			}
+		}
+
+		logrus.Infof("Changing configMap in namespace %s", pod.Namespace)
+		cm, err := core.Instance().GetConfigMap(statfsConfigMapName, pod.Namespace)
+		require.NoError(t, err)
+
+		// get the original golden values
+		origPreload, exists := cm.Data[cmDataPreloadKey]
+		require.True(t, exists)
+
+		origSum, exists := cm.Data[cmDataSumKey]
+		require.True(t, exists)
+
+		origSOFileContents, exists := cm.BinaryData[cmBinaryDataFileKey]
+		require.True(t, exists)
+
+		// change something in the configmap and verify that the configMap gets restored to the original golden values,
+		// when a new pod starts up
+		updateFn(cm)
+		_, err = core.Instance().UpdateConfigMap(cm)
+		require.NoError(t, err)
+
+		// verify that our changes went through
+		require.False(t, configMapMatches(pod.Namespace, origPreload, origSum, origSOFileContents))
+
+		// Delete a pod. When a new pod starts up, Stork webhook should detect the mismatch and update the configMap.
+		err = core.Instance().DeletePod(pod.Name, pod.Namespace, false)
+		require.NoError(t, err)
+
+		require.Eventuallyf(t, func() bool {
+			logrus.Infof("checking if the ConfigMap matches")
+			return configMapMatches(pod.Namespace, origPreload, origSum, origSOFileContents)
+		}, 3*time.Minute, 3*time.Second, "ConfigMap %s/%s did not get updated", pod.Namespace, statfsConfigMapName)
+	}
+	logrus.Infof("Validated configMap update in context %v", ctx.App.Key)
+}
+
+func configMapMatches(namespace, expectedPreload string, expectedSum string, expectedSOFileContents []byte) bool {
+	cm, err := core.Instance().GetConfigMap(statfsConfigMapName, namespace)
+	if err != nil {
+		logrus.Infof("Failed to get configmap %s/%s: %v", namespace, statfsConfigMapName, err)
+		return false
+	}
+
+	actualPreload := cm.Data[cmDataPreloadKey]
+	if actualPreload != expectedPreload {
+		logrus.Infof("ConfigMap.Data[%s] does not match: expected %s, actual %s",
+			cmDataPreloadKey, expectedPreload, actualPreload)
+		return false
+	}
+
+	actualSum := cm.Data[cmDataSumKey]
+	if actualSum != expectedSum {
+		logrus.Infof("ConfigMap.Data[%s] does not match: expected %s, actual %s", cmDataSumKey, expectedSum, actualSum)
+		return false
+	}
+
+	// .so file contents should match
+	if !bytes.Equal(expectedSOFileContents, cm.BinaryData[cmBinaryDataFileKey]) {
+		logrus.Infof("ConfigMap.BinaryData[%s] does not match: expected %v, actual %v",
+			cmBinaryDataFileKey, expectedSOFileContents, cm.BinaryData[cmBinaryDataFileKey])
+		return false
+	}
+	return true
+}
+
+// Returns a new []byte with 1-byte difference from the original one
+func updateBytesForTesting(origBytes []byte) []byte {
+	updatedBytes := append([]byte(nil), origBytes...)
+	updateIndex := len(updatedBytes) / 2
+	origByte := updatedBytes[updateIndex]
+	newByte := origByte ^ 1
+	logrus.Infof("Updating byte at index %d from %d to %d", updateIndex, origByte, newByte)
+	updatedBytes[updateIndex] = newByte
+	return updatedBytes
 }
