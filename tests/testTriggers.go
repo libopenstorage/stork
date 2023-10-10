1077cf5720c680e161bf916f1b814871b557d8ca
kphalgun-px <122591456+kphalgun-px@users.noreply.github.com>
2023-10-10 14:01:20 +0530

    [PTX-20334] Fix TriggerDeployNewApps (#1714)

--- tests/testTriggers.go
+++ tests/testTriggers.go
@@ -599,9 +599,8 @@ func TriggerDeployNewApps(contexts *[]*scheduler.Context, recordChan *chan *Even
 		for _, ctx := range *contexts {
 			log.Infof("Validating context: %v", ctx.App.Key)
 			ctx.SkipVolumeValidation = false
+			errorChan = make(chan error, errorChannelSize)
 			ValidateContext(ctx, &errorChan)
-			// BUG: Execution doesn't resume here after ValidateContext called
-			// Below code is never executed
 			for err := range errorChan {
 				log.Infof("Error: %v", err)
 				UpdateOutcome(event, err)
