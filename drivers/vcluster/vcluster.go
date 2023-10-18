4cff8eadfcde86d35699632d7caffec43d9807c1
Dhruv Bhatnagar <117347853+dbhatnagar-px@users.noreply.github.com>
2023-10-18 11:27:34 +0530

    Enabling Vcluster NodePort framework and one FIO Testcase (#1760)
    * Enabling Vcluster NodePort framework and one FIO Testcase
    
    * Adding comments for various methods
    
    * minor fixes
    
    * fixing cleanup
    
    * Incorporating Review comments
    
    * Incorporating review comments

--- drivers/vcluster/vcluster.go
+++ drivers/vcluster/vcluster.go
@@ -1,13 +1,32 @@
 package vcluster
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/ghodss/yaml"
+	"github.com/portworx/sched-ops/k8s/core"
+	"github.com/portworx/sched-ops/k8s/storage"
 	"github.com/portworx/sched-ops/task"
 	"github.com/portworx/torpedo/pkg/log"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
@@ -15,13 +34,44 @@ var (
 	CurrentClusterContext string
 	ContextChange         = false
 	NginxApp              = "nginx"
+	k8sCore               = core.Instance()
+	k8sStorage            = storage.Instance()
+	ControlNodeIP         string
 )
 
 const (
 	vClusterCreationTimeout   = 5 * time.Minute
 	VClusterRetryInterval     = 2 * time.Second
 	VclusterConnectionTimeout = 60 * time.Second
+	VclusterAppTimeout        = 30 * time.Minute
+	VClusterAppRetryInterval  = 30 * time.Second
 )
+
+type VCluster struct {
+	Namespace string
+	Name      string
+	NodePort  int32
+	Clientset *kubernetes.Clientset
+}
+
+type FIOOptions struct {
+	Name      string
+	IOEngine  string
+	RW        string
+	BS        string
+	NumJobs   int
+	Size      string
+	TimeBased bool
+	Runtime   string
+	Filename  string
+	EndFsync  int
+}
+
+// NewVCluster Creates instance of Vcluster
+func NewVCluster(name string) *VCluster {
+	namespace := "vcluster-" + name
+	return &VCluster{Namespace: namespace, Name: name}
+}
 
 // SwitchKubeContext This method switches kube context between host and any vcluster
 func SwitchKubeContext(target string) error {
@@ -76,10 +126,25 @@ func DeleteVCluster(vclusterName string) error {
 	return nil
 }
 
+// ExecuteVClusterCommand executes any generic vCluster command
+func ExecuteVClusterCommand(args ...string) (string, error) {
+	cmd := exec.Command("vcluster", args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed executing vcluster command with args %v: %w, Output: %s", args, err, output)
+	}
+	return string(output), nil
+}
+
+// TerminateVCluster Terminates Vcluster and runs it in its own context
+func (v *VCluster) TerminateVCluster() error {
+	_, err := ExecuteVClusterCommand("delete", v.Name)
+	return err
+}
+
 // CreateVCluster This method creates a vcluster. This requires vcluster.yaml saved in a specific location.
 func CreateVCluster(vclusterName string, absPath string) error {
-	cmd := exec.Command("vcluster", "create", vclusterName, "-f", absPath, "--connect=false")
-	err := cmd.Run()
+	_, err := ExecuteVClusterCommand("create", vclusterName, "-f", absPath, "--connect=false")
 	if err != nil {
 		return err
 	}
@@ -90,16 +155,385 @@ func CreateVCluster(vclusterName string, absPath string) error {
 // WaitForVClusterRunning This method waits for vcluster to come up in Running state and waits for a specific timeout to throw an error
 func WaitForVClusterRunning(vclusterName string, timeout time.Duration) error {
 	f := func() (interface{}, bool, error) {
-		cmd := exec.Command("vcluster", "list")
-		output, err := cmd.Output()
+		output, err := ExecuteVClusterCommand("list")
 		if err != nil {
 			return nil, true, err
 		}
-		if strings.Contains(string(output), vclusterName) && strings.Contains(string(output), "Running") {
+		if strings.Contains(output, vclusterName) && strings.Contains(output, "Running") {
 			return nil, false, nil
 		}
 		return nil, true, fmt.Errorf("Vcluster is not yet in running state")
 	}
 	_, err := task.DoRetryWithTimeout(f, vClusterCreationTimeout, VClusterRetryInterval)
 	return err
+}
+
+func CreateAndWaitForVCluster(vclusterCount int) ([]string, error) {
+	vclusterNames := make([]string, vclusterCount)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	controlIp, err := GetControlNodeIP()
+	if err != nil {
+		return nil, err
+	}
+	sampleVclusterConfig := filepath.Join(currentDir, "..", "..", "deployments", "customconfigs", "vcluster.yaml")
+	sampleVclusterConfigAbsPath, err := filepath.Abs(sampleVclusterConfig)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < vclusterCount; i++ {
+		vclusterNames[i] = fmt.Sprintf("my-vcluster%d", i+1)
+	}
+	for _, name := range vclusterNames {
+		vcluster_config_filename := "vcluster-" + name + ".yaml"
+		vClusterPath := filepath.Join(currentDir, "..", "..", "deployments", "customconfigs", vcluster_config_filename)
+		absPath, err := filepath.Abs(vClusterPath)
+		if err != nil {
+			return nil, err
+		}
+		err = UpdateVClusterConfig(sampleVclusterConfigAbsPath, absPath, controlIp)
+		if err != nil {
+			return nil, err
+		}
+		if err = CreateVCluster(name, absPath); err != nil {
+			return nil, err
+		}
+		if err = WaitForVClusterRunning(name, 10*time.Minute); err != nil {
+			return nil, err
+		}
+	}
+	return vclusterNames, nil
+}
+
+// GetControlNodeIP fetches the control node IP of host cluster to add it in vcluster config yaml file.
+func GetControlNodeIP() (string, error) {
+	nodes, err := k8sCore.GetNodes()
+	if err != nil {
+		return "", err
+	}
+	for _, node := range nodes.Items {
+		_, existsMaster := node.ObjectMeta.Labels["node-role.kubernetes.io/master"]
+		_, existsControlPlane := node.ObjectMeta.Labels["node-role.kubernetes.io/control-plane"]
+		if existsMaster || existsControlPlane {
+			for _, addr := range node.Status.Addresses {
+				if addr.Type == v1.NodeInternalIP {
+					return addr.Address, nil
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("control node IP not found")
+}
+
+// UpdateVClusterConfig creates a yaml file from original yaml file to suit the host cluster.
+// This yaml will be used to create a vcluster and connect to it later.
+func UpdateVClusterConfig(inputFile, outputFile, controlNodeIP string) error {
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+	var config map[string]interface{}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return err
+	}
+	syncer := map[string]interface{}{
+		"extraArgs": []string{"--tls-san=" + controlNodeIP},
+	}
+	config["syncer"] = syncer
+	updatedData, err := yaml.Marshal(config)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(outputFile, updatedData, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateAndWaitVCluster method creates and waits for vcluster
+func (v *VCluster) CreateAndWaitVCluster() error {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if ControlNodeIP == "" {
+		ControlNodeIP, err = GetControlNodeIP()
+		if err != nil {
+			return err
+		}
+	}
+	log.Infof("Control Node IP: %v", ControlNodeIP)
+	sampleVclusterConfig := filepath.Join(currentDir, "..", "..", "deployments", "customconfigs", "vcluster.yaml")
+	sampleVclusterConfigAbsPath, err := filepath.Abs(sampleVclusterConfig)
+	if err != nil {
+		return err
+	}
+
+	vcluster_config_filename := "vcluster-" + v.Name + ".yaml"
+	vClusterPath := filepath.Join(currentDir, "..", "..", "deployments", "customconfigs", vcluster_config_filename)
+	absPath, err := filepath.Abs(vClusterPath)
+	if err != nil {
+		return err
+	}
+	err = UpdateVClusterConfig(sampleVclusterConfigAbsPath, absPath, ControlNodeIP)
+	if err != nil {
+		return err
+	}
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: v.Namespace,
+		},
+	}
+	if _, err = k8sCore.CreateNamespace(ns); err != nil {
+		return err
+	}
+	if err = v.CreateNodePortService(); err != nil {
+		return err
+	}
+	if err = CreateVCluster(v.Name, absPath); err != nil {
+		return err
+	}
+	if err = v.SetClientSetForVCluster(); err != nil {
+		return err
+	}
+	if err = WaitForVClusterRunning(v.Name, 10*time.Minute); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateNodePortService Creates a Node Port service for vcluster context
+func (v *VCluster) CreateNodePortService() error {
+	service := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "vcluster-nodeport",
+			Namespace: v.Namespace,
+		},
+		Spec: v1.ServiceSpec{
+			Selector: map[string]string{
+				"app":     "vcluster",
+				"release": v.Name,
+			},
+			Ports: []v1.ServicePort{
+				{
+					Name:       "https",
+					Port:       443,
+					TargetPort: intstr.FromInt(8443),
+					Protocol:   v1.ProtocolTCP,
+				},
+			},
+			Type: v1.ServiceTypeNodePort,
+		},
+	}
+	createdSvc, err := k8sCore.CreateService(service)
+	if err != nil {
+		return err
+	}
+	v.NodePort = createdSvc.Spec.Ports[0].NodePort
+	return nil
+}
+
+// SetClientSetForVCluster method calculates the clientset for vcluster. This also
+// takes care of setting any extra params like skip-tls-verify, etc for vcluster
+func (v *VCluster) SetClientSetForVCluster() error {
+	serverURL := fmt.Sprintf("--server=https://%s:%d", ControlNodeIP, v.NodePort)
+	cmd := exec.Command("vcluster", "connect", v.Name, "-n", v.Namespace, "--update-current=false", serverURL)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	currentDir, _ := os.Getwd()
+	kcfPath := filepath.Join(currentDir, "kubeconfig.yaml")
+	config, err := clientcmd.BuildConfigFromFlags("", kcfPath)
+	if err != nil {
+		return err
+	}
+	// Bypass TLS verification for vCluster - Required for Nodeport way to connect
+	config.Insecure = true
+	config.TLSClientConfig.CAFile = ""
+	config.TLSClientConfig.CAData = nil
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	v.Clientset = clientset
+	return nil
+}
+
+// CreatePVC creates a PVC in vcluster
+func (v *VCluster) CreatePVC(svcName, appNs string) (string, error) {
+	pvcName := v.Name + "-" + svcName + "-pvc"
+	createOpts := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pvcName,
+			Namespace: appNs,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			StorageClassName: &svcName,
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse("5Gi"),
+				},
+			},
+		},
+	}
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: appNs,
+		},
+	}
+	// Creating Namespace in VCluster first before creating PVC
+	if _, err := v.Clientset.CoreV1().Namespaces().Create(context.TODO(), ns, metav1.CreateOptions{}); err != nil {
+		return "", err
+	}
+	_, err := v.Clientset.CoreV1().PersistentVolumeClaims(appNs).Create(context.TODO(), createOpts, metav1.CreateOptions{})
+	if err != nil {
+		return "", err
+	}
+	return pvcName, nil
+}
+
+// int32Ptr converts integer to pointer
+func int32Ptr(i int32) *int32 { return &i }
+
+// CreateFIODeployment creates a FIO Batch Job on given PVC
+func (v *VCluster) CreateFIODeployment(pvcName string, appNS string, fioOpts FIOOptions) error {
+	fioCmd := []string{
+		"fio",
+		"--name=" + fioOpts.Name,
+		"--ioengine=" + fioOpts.IOEngine,
+		"--rw=" + fioOpts.RW,
+		"--bs=" + fioOpts.BS,
+		"--numjobs=" + strconv.Itoa(fioOpts.NumJobs),
+		"--size=" + fioOpts.Size,
+		"--filename=" + fioOpts.Filename,
+		"--end_fsync=" + strconv.Itoa(fioOpts.EndFsync),
+	}
+	if fioOpts.TimeBased {
+		fioCmd = append(fioCmd, "--time_based")
+	}
+	fioCmd = append(fioCmd, "--runtime="+fioOpts.Runtime)
+	fioJob := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fio-job",
+			Namespace: appNS,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "fio",
+					},
+				},
+				Spec: corev1.PodSpec{
+					RestartPolicy: "Never",
+					Containers: []corev1.Container{
+						{
+							Name:    "fio-container",
+							Image:   "xridge/fio:latest",
+							Command: fioCmd,
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									MountPath: "/data",
+									Name:      "fio-volume",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "fio-volume",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: pvcName,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	log.Infof("Going ahead to run FIO Application on VCluster %v for %v ", v.Name, fioOpts.Runtime)
+	if _, err := v.Clientset.BatchV1().Jobs(appNS).Create(context.TODO(), fioJob, metav1.CreateOptions{}); err != nil {
+		return err
+	}
+	if err := v.WaitForFIOCompletion(appNS); err != nil {
+		return err
+	}
+
+	// Hard sleep to let fio pod finish up
+	time.Sleep(10 * time.Second)
+	pods, err := v.Clientset.CoreV1().Pods(appNS).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: labels.Set{"app": "fio"}.AsSelector().String(),
+	})
+	if err != nil {
+		return err
+	}
+	if len(pods.Items) == 0 {
+		return fmt.Errorf("no FIO pods found")
+	}
+	podName := pods.Items[0].Name
+	logs, err := v.FetchFIOLogs(podName, appNS)
+	if err != nil {
+		return err
+	}
+	log.Infof("Fio Output is: %v", logs)
+	return nil
+}
+
+// VClusterCleanup does all the cleanup related to vcluster tests
+func (v *VCluster) VClusterCleanup(scName string) error {
+	if err := v.TerminateVCluster(); err != nil {
+		return err
+	}
+	if err := k8sCore.DeleteNamespace(v.Namespace); err != nil {
+		return err
+	}
+	if err := k8sStorage.DeleteStorageClass(scName); err != nil {
+		return err
+	}
+	return nil
+}
+
+// WaitForFIOCompletion checks for FIO pod completion in vcluster context
+func (v *VCluster) WaitForFIOCompletion(namespace string) error {
+	f := func() (interface{}, bool, error) {
+		log.Infof("Entering to see if FIO Job has completed")
+		job, err := v.Clientset.BatchV1().Jobs(namespace).Get(context.TODO(), "fio-job", metav1.GetOptions{})
+		if err != nil {
+			return nil, false, err
+		}
+		if job.Status.Succeeded == 1 {
+			return nil, false, nil
+		}
+		return nil, true, fmt.Errorf("Still not completed. Looks like we have to wait for 30 seconds")
+	}
+	_, err := task.DoRetryWithTimeout(f, VclusterAppTimeout, VClusterAppRetryInterval)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// FetchFIOLogs method streams and fetches FIO pod logs in vcluster
+func (v *VCluster) FetchFIOLogs(podName, namespace string) (string, error) {
+	podLogOpts := corev1.PodLogOptions{Container: "fio-container"}
+	request := v.Clientset.CoreV1().Pods(namespace).GetLogs(podName, &podLogOpts)
+	podLogs, err := request.Stream(context.TODO())
+	if err != nil {
+		return "", err
+	}
+	defer podLogs.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogs)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
--- tests/basic/vcluster_test.go
+++ tests/basic/vcluster_test.go
@@ -4,56 +4,89 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/portworx/sched-ops/k8s/storage"
 	"github.com/portworx/sched-ops/task"
+	"github.com/portworx/torpedo/drivers/scheduler"
+	"github.com/portworx/torpedo/drivers/scheduler/k8s"
 	"github.com/portworx/torpedo/drivers/vcluster"
 	"github.com/portworx/torpedo/pkg/log"
 	. "github.com/portworx/torpedo/tests"
+
+	v1 "k8s.io/api/core/v1"
+	storageApi "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("CreateNginxAppOnVcluster", func() {
+var _ = Describe("CreateAndRunFioOnVcluster", func() {
+	vc := &vcluster.VCluster{}
+	var scName string
+	var pvcName string
+	var appNS string
+	fioOptions := vcluster.FIOOptions{
+		Name:      "mytest",
+		IOEngine:  "libaio",
+		RW:        "randwrite",
+		BS:        "4k",
+		NumJobs:   1,
+		Size:      "500m",
+		TimeBased: true,
+		Runtime:   "600s",
+		Filename:  "/data/fiotest",
+		EndFsync:  1,
+	}
 	JustBeforeEach(func() {
-		StartTorpedoTest("VclusterOperations", "Create, Connect and execute a method on Vcluster", nil, 0)
+		StartTorpedoTest("CreateAndRunFioOnVcluster", "Create, Connect and run FIO Application on Vcluster", nil, 0)
+		vc = vcluster.NewVCluster("my-vcluster1")
+		err := vc.CreateAndWaitVCluster()
+		log.FailOnError(err, "Failed to create VCluster")
 	})
-	var vclusterNames = []string{"my-vcluster1"}
-	It("Create and connect to vclusters and run a sample method", func() {
-		steplog := "Create vClusters"
-		log.InfoD(steplog)
-		Step(steplog, func() {
-			for _, name := range vclusterNames {
-				currentDir, err := os.Getwd()
-				log.FailOnError(err, "Could not get absolute path to current Dir")
-				vClusterPath := filepath.Join(currentDir, "..", "deployments", "customconfigs", "vcluster.yaml")
-				absPath, err := filepath.Abs(vClusterPath)
-				log.FailOnError(err, "Could not get absolute path to vcluster.yaml")
-				err = vcluster.CreateVCluster(name, absPath)
-				log.FailOnError(err, "Failed to create vCluster")
-			}
-		})
-		steplog = "Wait for all vClusters to come up in Running State"
-		log.InfoD(steplog)
-		Step(steplog, func() {
-			for _, name := range vclusterNames {
-				err := vcluster.WaitForVClusterRunning(name, 10*time.Minute)
-				log.FailOnError(err, "Vcluster did not come up in time")
-			}
-		})
-		steplog = "Connect to each vCluster and execute a method"
-		log.InfoD(steplog)
-		Step(steplog, func() {
-			for _, name := range vclusterNames {
-				log.Infof("Trying to connect to %v", name)
-				err := ConnectVClusterAndExecute(name, RunNginxAppOnVcluster, name, vcluster.NginxApp)
-				log.FailOnError(err, fmt.Sprintf("Failed to connect and execute method on cluster %v", name))
-			}
-		})
+	It("Create FIO app on VCluster and run it for 10 minutes", func() {
+		// Create Storage Class on Host Cluster
+		scName = fmt.Sprintf("fio-app-sc-%v", time.Now().Unix())
+		err = CreateStorageClass(scName)
+		log.FailOnError(err, "Error creating Storageclass")
+		log.Infof("Successfully created StorageClass with name: %v", scName)
+		// Create PVC on VCluster
+		appNS = scName + "-ns"
+		pvcName, err = vc.CreatePVC(scName, appNS)
+		log.FailOnError(err, fmt.Sprintf("Error creating PVC with Storageclass name %v", scName))
+		log.Infof("Successfully created PVC with name: %v", pvcName)
+		// Create FIO Deployment on VCluster using the above PVC
+		err = vc.CreateFIODeployment(pvcName, appNS, fioOptions)
+		log.FailOnError(err, "Error in creating FIO Application")
+		log.Infof("Successfully ran FIO on Vcluster")
 	})
 	JustAfterEach(func() {
-		defer EndTorpedoTest()
+		// VCluster, StorageClass and Namespace cleanup
+		err := vc.VClusterCleanup(scName)
+		if err != nil {
+			log.Errorf("Problem in Cleanup: %v", err)
+		} else {
+			log.Infof("Cleanup successfully done.")
+		}
+	})
+})
+
+var _ = Describe("CreateNginxAppOnVcluster", func() {
+	const vclusterCount = 1
+	vclusterNames := make([]string, vclusterCount)
+	JustBeforeEach(func() {
+		StartTorpedoTest("VclusterOperations", "Create, Connect and execute a method on Vcluster", nil, 0)
+		vclusterNames, err = vcluster.CreateAndWaitForVCluster(vclusterCount)
+		log.FailOnError(err, "Failed in vcluster prep")
+	})
+	It("Bring up Nginx Application", func() {
+		for _, name := range vclusterNames {
+			log.Infof("Trying to connect to %v", name)
+			err := ConnectVClusterAndExecute(name, RunNginxAppOnVcluster, name, vcluster.NginxApp)
+			log.FailOnError(err, fmt.Sprintf("Failed to connect and execute method on cluster %v", name))
+		}
+	})
+	JustAfterEach(func() {
 		for _, name := range vclusterNames {
 			vcluster.DeleteVCluster(name)
 		}
@@ -136,5 +169,36 @@ func RunNginxAppOnVcluster(args []interface{}) []interface{} {
 	if len(errStrings) > 0 {
 		return []interface{}{errStrings}
 	}
+	opts := make(map[string]bool)
+	opts[scheduler.OptionsWaitForResourceLeakCleanup] = true
+
+	for _, ctx := range context {
+		TearDownContext(ctx, opts)
+	}
 	return []interface{}{nil}
+}
+
+// CreateStorageClass method creates a storageclass using host's k8s clientset on host cluster
+func CreateStorageClass(scName string) error {
+	params := make(map[string]string)
+	params["repl"] = "2"
+	params["priority_io"] = "high"
+	params["io_profile"] = "auto"
+	v1obj := metav1.ObjectMeta{
+		Name: scName,
+	}
+	reclaimPolicyDelete := v1.PersistentVolumeReclaimDelete
+	bindMode := storageApi.VolumeBindingImmediate
+	scObj := storageApi.StorageClass{
+		ObjectMeta:        v1obj,
+		Provisioner:       k8s.CsiProvisioner,
+		Parameters:        params,
+		ReclaimPolicy:     &reclaimPolicyDelete,
+		VolumeBindingMode: &bindMode,
+	}
+	k8sStorage := storage.Instance()
+	if _, err := k8sStorage.CreateStorageClass(&scObj); err != nil {
+		return err
+	}
+	return nil
 }
