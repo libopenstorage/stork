16d9bbea180eb8cc59acc368c0abe753ad484015
Aniket Ghodke <89044246+aghodke1312@users.noreply.github.com>
2023-10-31 14:14:08 -0700

    Adding test for volume in detached state (#1831)

--- tests/basic/autopilot_test.go
+++ tests/basic/autopilot_test.go
@@ -117,6 +117,72 @@ var _ = Describe(fmt.Sprintf("{%sPvcBasic}", testSuiteName), func() {
 	})
 })
 
+var autopilotPVCRule = []apapi.AutopilotRule{
+	aututils.PVCRuleByTotalSize(10, 100, "20Gi"),
+}
+
+// This testsuite is used applying autopilot rules on a detached volume and validates the size of PVC
+var _ = Describe(fmt.Sprintf("{%sPVCVolDetached}", testSuiteName), func() {
+
+	JustBeforeEach(func() {
+		StartTorpedoTest(fmt.Sprintf("{%sPVCVolDetached}", testSuiteName), "Perform basic scenarios with Autopilot with detached volume", nil, 0)
+	})
+	var contexts []*scheduler.Context
+	It("has to, resize the volume which is in detached state, validate and teardown apps", func() {
+		testName := strings.ToLower(fmt.Sprintf("%sPVCVolDetached", testSuiteName))
+
+		Step("schedule applications", func() {
+			for i := 0; i < Inst().GlobalScaleFactor; i++ {
+				for id, apRule := range autopilotPVCRule {
+					taskName := fmt.Sprintf("%s-%d-aprule%d", testName, i, id)
+					apRule.Name = fmt.Sprintf("%s-%d", apRule.Name, i)
+					labels := map[string]string{
+						"autopilot": apRule.Name,
+					}
+					apRule.Spec.ActionsCoolDownPeriod = int64(60)
+					context, err := Inst().S.Schedule(taskName, scheduler.ScheduleOptions{
+						AppKeys:            Inst().AppList,
+						StorageProvisioner: Inst().Provisioner,
+						AutopilotRule:      apRule,
+						Labels:             labels,
+					})
+					Expect(err).NotTo(HaveOccurred())
+					Expect(context).NotTo(BeEmpty())
+					contexts = append(contexts, context...)
+				}
+			}
+		})
+
+		Step("validating volumes and verifying size of volumes", func() {
+			for _, ctx := range contexts {
+				ValidateVolumes(ctx)
+			}
+		})
+
+		Step(fmt.Sprintf("wait for unscheduled resize of volume (%s)", unscheduledResizeTimeout), func() {
+			time.Sleep(unscheduledResizeTimeout)
+		})
+
+		Step("validating volumes and verifying size of volumes", func() {
+			for _, ctx := range contexts {
+				ValidateVolumes(ctx)
+			}
+		})
+
+		Step("destroy apps", func() {
+			opts := make(map[string]bool)
+			opts[scheduler.OptionsWaitForResourceLeakCleanup] = true
+			for _, ctx := range contexts {
+				TearDownContext(ctx, opts)
+			}
+		})
+	})
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(contexts)
+	})
+})
+
 // This testsuite is used for performing basic scenarios with Autopilot rules where it
 // schedules apps and wait until workload is completed on the volumes. Restarts volume
 // driver and validates PVC sizes of the volumes
