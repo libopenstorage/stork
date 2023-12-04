ae43da2449e81d42b6290171c45b490751e13b29
vprabhakar-px <120549478+vprabhakar-px@users.noreply.github.com>
2023-12-04 12:47:43 +0530

    fix for HAIncreaseAndCrashPX (#1941)

--- tests/basic/longevity_test.go
+++ tests/basic/longevity_test.go
@@ -911,6 +911,8 @@ func populateIntervals() {
 	triggerInterval[AddStorageNode] = make(map[int]time.Duration)
 	triggerInterval[AddStoragelessNode] = make(map[int]time.Duration)
 	triggerInterval[OCPStorageNodeRecycle] = make(map[int]time.Duration)
+	triggerInterval[HAIncreaseAndCrashPX] = make(map[int]time.Duration)
+	triggerInterval[HAIncreaseAndRestartPX] = make(map[int]time.Duration)
 
 	baseInterval := 10 * time.Minute
 
