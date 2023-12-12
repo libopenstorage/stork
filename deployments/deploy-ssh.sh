a7515593bfdc9b751ea442a7c57596448cff2338
kphalgun-px <122591456+kphalgun-px@users.noreply.github.com>
2023-12-12 13:52:58 +0530

    Handle Jenkins SIGTERM (#1939)

--- deployments/deploy-ssh.sh
+++ deployments/deploy-ssh.sh
@@ -747,32 +747,54 @@ function describe_pod_then_exit {
   exit 1
 }
 
-first_iteration=true
-
-for i in $(seq 1 900); do
-  echo "Iteration: $i"
-  state=$(kubectl get pod torpedo | grep -v NAME | awk '{print $3}')
-
-  if [ "$state" == "Error" ]; then
-    echo "Error: Torpedo finished with $state state"
-    describe_pod_then_exit
-  elif [ "$state" == "Running" ]; then
-    # For the first iteration, display all logs. Later, only from 1 minute ago
-    if [ "$first_iteration" = true ]; then
-      echo "Logs from first iteration"
-      kubectl logs -f torpedo
-      first_iteration=false
-    else
-      echo "Logs from iteration: $i"
-      kubectl logs -f --since=1m torpedo
+function terminate_pod_then_exit {
+    echo "Terminating Ginkgo test in Torpedo pod..."
+    # Fetch the PID of the Ginkgo test process
+    local test_pid
+    test_pid=$(kubectl exec torpedo -- pgrep -f 'torpedo/bin')
+    if [ "$test_pid" ]; then
+        # Using SIGKILL instead of SIGTERM to immediately stop the process.
+        # SIGTERM would allow Ginkgo to run AfterSuite and generate reports,
+        # but the intention here is to stop the process immediately.
+        echo "Sending SIGKILL to terminate Ginkgo test process with PID: $test_pid"
+        kubectl exec torpedo -- kill -SIGKILL "$test_pid"
     fi
-  elif [ "$state" == "Completed" ]; then
-    echo "Success: Torpedo finished with $state state"
-    exit 0
-  fi
+    exit 1
+}
 
-  sleep 1
-done
+trap terminate_pod_then_exit SIGTERM
 
-echo "Error: Failed to wait for torpedo to start running..."
-describe_pod_then_exit
+# The for loop is run in a background process (subshell) to allow the main script
+# to remain responsive to signals (SIGTERM, SIGINT) while the loop is executing.
+(
+    first_iteration=true
+    for i in $(seq 1 900); do
+        echo "Iteration: $i"
+        state=$(kubectl get pod torpedo | grep -v NAME | awk '{print $3}')
+
+        if [ "$state" == "Error" ]; then
+            echo "Error: Torpedo finished with $state state"
+            describe_pod_then_exit
+        elif [ "$state" == "Running" ]; then
+            # For the first iteration, display all logs. Later, only from 1 minute ago
+            if [ "$first_iteration" = true ]; then
+                echo "Logs from first iteration"
+                kubectl logs -f torpedo
+                first_iteration=false
+            else
+                echo "Logs from iteration: $i"
+                kubectl logs -f --since=1m torpedo
+            fi
+        elif [ "$state" == "Completed" ]; then
+            echo "Success: Torpedo finished with $state state"
+            exit 0
+        fi
+
+        sleep 1
+    done
+
+    echo "Error: Failed to wait for torpedo to start running..."
+    describe_pod_then_exit
+) &
+
+wait $!
