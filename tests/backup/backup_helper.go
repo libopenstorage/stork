9f9934a2abcf797491f8b6d95b8d1617522f0d2f
atrivedi-px <149039428+atrivedi-px@users.noreply.github.com>
2023-12-16 16:28:42 +0530

    Skipping CR cleanup validation in case of Sync Backups (#1976)
    * Skipping validation in case of Sync Backups
    
    * Adding doc strings
    
    ---------
    
    Co-authored-by: atrivedi-px <atrivedi-px@purestorage.com>

--- tests/backup/backup_helper.go
+++ tests/backup/backup_helper.go
@@ -6364,8 +6364,6 @@ func dumpMongodbCollectionOnConsole(kubeConfigFile string, collectionName string
 // validateCRCleanup validates CR cleanup created during backup or restore
 func validateCRCleanup(resourceInterface interface{},
 	ctx context.Context) error {
-	log.InfoD("Inside validateCRCleanup")
-	log.Infof("Resource Interface - [%+v]", resourceInterface)
 	var allCRs []string
 	var err error
 	var getCRMethod func(string, *api.ClusterObject) ([]string, error)
@@ -6373,6 +6371,7 @@ func validateCRCleanup(resourceInterface interface{},
 	var resourceNamespaces []string
 	var resourceName string
 	var orgID string
+	var isValidCluster = false
 
 	if currentObject, ok := resourceInterface.(*api.BackupObject); ok {
 		// Creating object and variables from backup object
@@ -6390,6 +6389,29 @@ func validateCRCleanup(resourceInterface interface{},
 		}
 		orgID = currentObject.OrgId
 		resourceName = currentObject.Name
+	}
+
+	// Below code is added to skip the CR cleanup validation in case of synced backup
+	// For synced backup the cluster name has uuid suffix which is not supported/handled
+	// While creating the cluster object
+
+	// Fetching all clusters
+	enumerateClusterRequest := &api.ClusterEnumerateRequest{
+		OrgId: orgID,
+	}
+	enumerateClusterResponse, err := Inst().Backup.EnumerateAllCluster(ctx, enumerateClusterRequest)
+
+	// Comparing cluster names to the name from backup inspect response
+	for _, clusterObj := range enumerateClusterResponse.GetClusters() {
+		if clusterObj.Name == clusterName {
+			isValidCluster = true
+			break
+		}
+	}
+
+	if !isValidCluster {
+		log.Infof("%s looks to be a synced backup, skipping CR cleanup validation", clusterName)
+		return nil
 	}
 
 	backupDriver := Inst().Backup
