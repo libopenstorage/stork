e9f39b7f85d50798519a579900a9d773a5934ef6
sn-px <121086405+sn-px@users.noreply.github.com>
2023-12-20 10:13:40 +0530

    Adding volume driver support for non-px  gke (#1981)
    * Adding support for gce volume driver

--- drivers/volume/gce/gce.go
+++ drivers/volume/gce/gce.go
@@ -8,18 +8,21 @@ import (
 
 	torpedovolume "github.com/portworx/torpedo/drivers/volume"
 	"github.com/portworx/torpedo/drivers/volume/portworx/schedops"
+	"github.com/portworx/torpedo/pkg/errors"
 )
 
 const (
 	// DriverName is the name of the gce driver implementation
 	DriverName = "gce"
 	// GceStorage GCE storage driver name
-	GceStorage torpedovolume.StorageProvisionerType = "gce"
+	GceStorage         torpedovolume.StorageProvisionerType = "gce-pd"
+	GceStandardStorage torpedovolume.StorageProvisionerType = "pd-csi-storage-gke"
 )
 
 // Provisioners types of supported provisioners
 var provisioners = map[torpedovolume.StorageProvisionerType]torpedovolume.StorageProvisionerType{
-	GceStorage: "kubernetes.io/gce-pd",
+	GceStorage:         "kubernetes.io/gce-pd",
+	GceStandardStorage: "pd.csi.storage.gke.io",
 }
 
 type gce struct {
@@ -28,7 +31,7 @@ type gce struct {
 }
 
 func (d *gce) String() string {
-	return string(GceStorage)
+	return string(DriverName)
 }
 
 func (d *gce) GetProxySpecForAVolume(volume *torpedovolume.Volume) (*api.ProxySpec, error) {
@@ -57,6 +60,45 @@ func (d *gce) Init(sched, nodeDriver, token, storageProvisioner, csiGenericDrive
 	return nil
 }
 
+func (d *gce) ValidateCreateVolume(name string, params map[string]string) error {
+	// TODO: Implementation of ValidateCreateVolume will be provided in the coming PRs
+	log.Warnf("ValidateCreateVolume function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) ValidateVolumeSetup(vol *torpedovolume.Volume) error {
+	// TODO: Implementation of ValidateVolumeSetup will be provided in the coming PRs
+	log.Warnf("ValidateVolumeSetup function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) ValidateDeleteVolume(vol *torpedovolume.Volume) error {
+	// TODO: Implementation of ValidateDeleteVolume will be provided in the coming PRs
+	log.Warnf("ValidateDeleteVolume function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) GetDriverVersion() (string, error) {
+	// TODO: Implementation of ValidateDeleteVolume will be provided in the coming PRs
+	log.Warnf("GetDriverVersion function has not been implemented for volume driver - %s", d.String())
+	return "", nil
+}
+
+// RefreshDriverEndpoints get the updated driver endpoints for the cluster
+func (d *gce) RefreshDriverEndpoints() error {
+
+	log.Warnf("RefreshDriverEndpoints function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+// InspectVolume inspects the volume with the given name
+func (d *gce) InspectVolume(name string) (*api.Volume, error) {
+	log.Warnf("InspectVolume function has not been implemented for volume driver - %s", d.String())
+	return nil, &errors.ErrNotSupported{
+		Type:      "Function",
+		Operation: "InspectVolume()",
+	}
+}
 func init() {
 	torpedovolume.Register(DriverName, provisioners, &gce{})
 }
