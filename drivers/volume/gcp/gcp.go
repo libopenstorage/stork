ddfc9dfdb756101a84484007bbcccc1cd7cd8213
strivedi-px <strivedi@purestorage.com>
2024-02-07 12:41:17 +0530

    chore: log namespace with the resource that causes backup or restore to fail

--- drivers/volume/gcp/gcp.go
+++ drivers/volume/gcp/gcp.go
@@ -129,7 +129,7 @@ func (g *gcp) OwnsPVC(coreOps core.Ops, pvc *v1.PersistentVolumeClaim) bool {
 		// Try to get info from the PV since storage class could be deleted
 		pv, err := coreOps.GetPersistentVolume(pvc.Spec.VolumeName)
 		if err != nil {
-			logrus.Warnf("Error getting pv %v for pvc %v: %v", pvc.Spec.VolumeName, pvc.Name, err)
+			logrus.Warnf("Error getting pv %v for pvc %v in namespace %s: %v", pvc.Spec.VolumeName, pvc.Name, pvc.Namespace, err)
 			return false
 		}
 		return g.OwnsPV(pv)
--- drivers/volume/linstor/linstor.go
+++ drivers/volume/linstor/linstor.go
@@ -364,7 +364,7 @@ func (l *linstor) OwnsPVC(coreOps core.Ops, pvc *v1.PersistentVolumeClaim) bool 
 		// Try to get info from the PV since storage class could be deleted
 		pv, err := coreOps.GetPersistentVolume(pvc.Spec.VolumeName)
 		if err != nil {
-			logrus.Warnf("Error getting pv %v for pvc %v: %v", pvc.Spec.VolumeName, pvc.Name, err)
+			logrus.Warnf("Error getting pv %v for pvc %v in namespace %s: %v", pvc.Spec.VolumeName, pvc.Name, pvc.Namespace, err)
 			return false
 		}
 		return l.OwnsPV(pv)
