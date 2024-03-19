ab314d1a9a6b2c09e93b8e658627a178f949c41f
vprabhakar-px <120549478+vprabhakar-px@users.noreply.github.com>
2024-03-19 18:21:46 +0530

    increase stats update time from 2 min to 5 min (#2184)

--- tests/common.go
+++ tests/common.go
@@ -1372,7 +1372,7 @@ func ValidatePureVolumeStatisticsDynamicUpdate(ctx *scheduler.Context, errChan .
 			processError(err, errChan...)
 			fmt.Println("sleeping to let volume usage get reflected")
 			// wait until the backends size is reflected before making the REST call
-			time.Sleep(time.Minute * 2)
+			time.Sleep(time.Minute * 5)
 
 			byteUsedAfter, err := Inst().V.ValidateGetByteUsedForVolume(vols[0].ID, make(map[string]string))
 			fmt.Printf("after writing random bytes to the file the byteUsed in volume %s is %v\n", vols[0].ID, byteUsedAfter)
