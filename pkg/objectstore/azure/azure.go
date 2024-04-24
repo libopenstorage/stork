b7d57142524e82ca36495fb116381acd53e10089
shkumari-px <141733960+shkumari-px@users.noreply.github.com>
2024-04-24 22:06:29 +0530

    pb-6675: Add support to use AZURE environment variable in the azure (#1727)
    objectstore apis.
    
    	- Added a field in azure config of backuplocation CR, to get the
    	  Azure Environment value.
    	- Modified the azure objectstore api to take the azure
    	  environment variable from BL CR and env variable, if it is present.
    
    Co-authored-by: sivakumar subraani <sivakumar subramani>

--- pkg/objectstore/azure/azure.go
+++ pkg/objectstore/azure/azure.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"os"
 
 	"github.com/Azure/azure-pipeline-go/pipeline"
 	"github.com/Azure/azure-storage-blob-go/azblob"
+	az_autorest "github.com/Azure/go-autorest/autorest/azure"
 	stork_api "github.com/libopenstorage/stork/pkg/apis/stork/v1alpha1"
 	"github.com/libopenstorage/stork/pkg/objectstore/common"
 	"github.com/sirupsen/logrus"
 	"gocloud.dev/blob"
 	"gocloud.dev/blob/azureblob"
+)
+
+const (
+	azureEnvKey = "AZURE_ENVIRONMENT"
 )
 
 func getPipeline(backupLocation *stork_api.BackupLocation) (pipeline.Pipeline, error) {
@@ -24,6 +30,31 @@ func getPipeline(backupLocation *stork_api.BackupLocation) (pipeline.Pipeline, e
 	return azureblob.NewPipeline(credential, azblob.PipelineOptions{}), nil
 }
 
+func getAzureURLSuffix(backupLocation *stork_api.BackupLocation) (string, error) {
+	// Give first preference to environment variable setting.
+	azureEnv := os.Getenv(azureEnvKey)
+	if len(azureEnv) == 0 {
+		// If env variable is not set, check the azure environment vaue from backuplocation CR.
+		if len(backupLocation.Location.AzureConfig.Environment) != 0 {
+			azureEnv = string(backupLocation.Location.AzureConfig.Environment)
+			logrus.Infof("Received azure environment type %s from backup location cr", azureEnv)
+		} else {
+			logrus.Infof("Both BL Cr environment type and azureEnv are empty, setting storage domain to default i.e Public")
+			return az_autorest.PublicCloud.StorageEndpointSuffix, nil
+		}
+	} else {
+		logrus.Infof("Received azure environment type as environment variable which has higher priority than environment type passed down from backup location cr %s", azureEnv)
+	}
+
+	azureClientEnv, err := az_autorest.EnvironmentFromName(azureEnv)
+	if err != nil {
+		logrus.Errorf("Failed to get azure client env for:%v, err:%v", azureEnv, err)
+		return "", err
+	}
+	// Endpoint suffix based on current cloud type
+	return azureClientEnv.StorageEndpointSuffix, nil
+}
+
 // GetBucket gets a reference to the bucket for that backup location
 func GetBucket(backupLocation *stork_api.BackupLocation) (*blob.Bucket, error) {
 	accountName := azureblob.AccountName(backupLocation.Location.AzureConfig.StorageAccountName)
@@ -31,7 +62,17 @@ func GetBucket(backupLocation *stork_api.BackupLocation) (*blob.Bucket, error) {
 	if err != nil {
 		return nil, err
 	}
-	return azureblob.OpenBucket(context.Background(), pipeline, accountName, backupLocation.Location.Path, nil)
+	urlSuffix, err := getAzureURLSuffix(backupLocation)
+	if err != nil {
+		return nil, err
+	}
+
+	urlSuffix = fmt.Sprintf("blob.%s", urlSuffix)
+	logrus.Debugf("azure - GetBucket - urlSuffix %v", urlSuffix)
+	opts := azureblob.Options{
+		StorageDomain: azureblob.StorageDomain(urlSuffix),
+	}
+	return azureblob.OpenBucket(context.Background(), pipeline, accountName, backupLocation.Location.Path, &opts)
 }
 
 // CreateBucket creates a bucket for the bucket location
@@ -41,11 +82,16 @@ func CreateBucket(backupLocation *stork_api.BackupLocation) error {
 	if err != nil {
 		return err
 	}
-	url, err := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net", accountName))
+	urlSuffix, err := getAzureURLSuffix(backupLocation)
 	if err != nil {
 		return err
 	}
 
+	url, err := url.Parse(fmt.Sprintf("https://%s.blob.%s", accountName, urlSuffix))
+	if err != nil {
+		return err
+	}
+	logrus.Debugf("azure - CreateBucket - url %v", url)
 	_, err = azblob.NewServiceURL(*url, pipeline).
 		NewContainerURL(backupLocation.Location.Path).
 		Create(context.Background(), azblob.Metadata{}, azblob.PublicAccessNone)
