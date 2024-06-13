abd64d9ebe63853003ff539d81ebb4754f4c1d57
Lalatendu Das <ldas@purestorage.com>
2024-06-13 19:40:33 +0000

    pb-6910: add uid & gid in each volume for psa support
    - If PSA is enabled and a volume is backed-up with a specific Uid/Gid
      then that value need to be preserved for being used during restore process.
    - This is needed for all the job PODS to inherit above values so that no permission
      denied issues appear while restoring from a file backup
    - OCP enforces a different UID GID range due to its mutating webhook, hence added
      logic to use those valid ranges instead of utilizing preserved UID/GID during backup
    
    Signed-off-by: Lalatendu Das <ldas@purestorage.com>

--- drivers/volume/kdmp/kdmp.go
+++ drivers/volume/kdmp/kdmp.go
@@ -234,6 +234,13 @@ func (k *kdmp) StartBackup(backup *storkapi.ApplicationBackup,
 		if err != nil {
 			return nil, fmt.Errorf("error getting pv %v: %v", pvName, err)
 		}
+		// Get UID and GID from the App which is controlling this PVC.
+		getUIDFromApp := true
+		podUserId, podGroupId, err := utils.GetPsaEnabledAppUID(pvc.Name, pvc.Namespace, backup, getUIDFromApp)
+		if err != nil {
+			logrus.Errorf("failed to get the UID and GID for pvc %s %v", pvc.Name, err)
+			return nil, err
+		}
 		volumeInfo := &storkapi.ApplicationBackupVolumeInfo{}
 		zones, err := getZones(pv)
 		if err != nil {
@@ -249,6 +256,13 @@ func (k *kdmp) StartBackup(backup *storkapi.ApplicationBackup,
 		volumeInfo.StorageClass = k8shelper.GetPersistentVolumeClaimClass(&pvc)
 		volumeInfo.Namespace = pvc.Namespace
 		volumeInfo.DriverName = storkvolume.KDMPDriverName
+		// if psaIsenabled then set the a UID GID in the VolumeInfo and
+		// set annotation in dataexport CR for updating the JOB spec
+		logrus.Infof("Setting UID in VolumeInfo for PVC %s", pvc.Name)
+		volumeInfo.JobSecurityContext.RunAsUser = podUserId
+		logrus.Infof("Setting GID in VolumeInfo for PVC %s", pvc.Name)
+		volumeInfo.JobSecurityContext.RunAsGroup = podGroupId
+
 		volume, err := core.Instance().GetVolumeForPersistentVolumeClaim(&pvc)
 		if err != nil {
 			return nil, fmt.Errorf("error getting volume for PVC: %v", err)
@@ -285,6 +299,12 @@ func (k *kdmp) StartBackup(backup *storkapi.ApplicationBackup,
 		dataExport.Annotations[utils.BackupObjectUIDKey] = string(backup.Annotations[utils.PxbackupObjectUIDKey])
 		dataExport.Annotations[pvcUIDKey] = string(pvc.UID)
 		dataExport.Annotations[kdmpStorageClassKey] = volumeInfo.StorageClass
+		if podUserId != utils.UndefinedId {
+			dataExport.Annotations[utils.PsaUIDKey] = fmt.Sprintf("%d", podUserId)
+		}
+		if podGroupId != utils.UndefinedId {
+			dataExport.Annotations[utils.PsaGIDKey] = fmt.Sprintf("%d", podGroupId)
+		}
 		dataExport.Name = getGenericCRName(utils.PrefixBackup, string(backup.UID), string(pvc.UID), pvc.Namespace)
 		dataExport.Namespace = pvc.Namespace
 		dataExport.Spec.Type = kdmpapi.DataExportKopia
@@ -787,6 +807,12 @@ func (k *kdmp) StartRestore(
 			msg := fmt.Sprintf("unable to find backup uid from applicationbackup %s/%s", restore.Namespace, restore.Spec.BackupName)
 			return nil, fmt.Errorf(msg)
 		}
+		getUIDFromApp := false
+		podUserId, podGroupId, err := utils.GetPsaEnabledAppUID(pvc.Name, bkpvInfo.Namespace, backup, getUIDFromApp)
+		if err != nil {
+			logrus.Errorf("failed to get the UID and GID for pvc %s %v", pvc.Name, err)
+			return nil, err
+		}
 		backupUID = backup.Annotations[backupUIDKey]
 		// create kdmp cr
 		dataExport := &kdmpapi.DataExport{}
@@ -802,6 +828,12 @@ func (k *kdmp) StartRestore(
 		dataExport.Annotations[utils.SkipResourceAnnotation] = "true"
 		dataExport.Annotations[utils.BackupObjectUIDKey] = backupUID
 		dataExport.Annotations[pvcUIDKey] = bkpvInfo.PersistentVolumeClaimUID
+		if podUserId != utils.UndefinedId {
+			dataExport.Annotations[utils.PsaUIDKey] = fmt.Sprintf("%d", podUserId)
+		}
+		if podGroupId != utils.UndefinedId {
+			dataExport.Annotations[utils.PsaGIDKey] = fmt.Sprintf("%d", podGroupId)
+		}
 		dataExport.Name = getGenericCRName(prefixRestore, string(restore.UID), bkpvInfo.PersistentVolumeClaimUID, restoreNamespace)
 		dataExport.Namespace = restoreNamespace
 		dataExport.Status.TransferID = volBackup.Namespace + "/" + volBackup.Name
