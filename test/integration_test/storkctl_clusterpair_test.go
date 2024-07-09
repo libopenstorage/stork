18a91358483d4b75c9cb1ab9cb3a873445c671af
strivedi-px <strivedi@purestorage.com>
2024-07-09 11:51:56 +0530

    test: use --mode migration for running migration once

--- test/integration_test/storkctl_clusterpair_test.go
+++ test/integration_test/storkctl_clusterpair_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/pborman/uuid"
 	"github.com/portworx/sched-ops/k8s/core"
 	"github.com/portworx/sched-ops/k8s/stork"
+	"github.com/portworx/torpedo/drivers/scheduler"
 	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 )
 
@@ -61,6 +62,7 @@ func testStorkCtlClusterPairNFSBidirectional(t *testing.T) {
 		"nfs-server":      nfsSrvAddr,
 		"nfs-export-path": nfsSrvExpPath,
 		"nfs-sub-path":    fmt.Sprintf("test-%s", uuid.New()),
+		"mode":            "migration",
 	}
 
 	// Execute the storkctl command.
@@ -91,11 +93,36 @@ func testStorkCtlClusterPairNFSBidirectional(t *testing.T) {
 		Dash.Fatal("cluster pair creation failed, expected success got=%s", actualOutput)
 	}
 
+	///////////////////////////////////////
+	// Deploy app for running migration //
+	//////////////////////////////////////
+	err = setSourceKubeConfig()
+	log.FailOnError(t, err, "Error resetting source config")
+	instanceID := "nfs-bidirectional-cp-test"
+	appKey := "mysql-1-pvc"
+
+	// Schedule mysql replicas.
+	ctxs, err := schedulerDriver.Schedule(instanceID,
+		scheduler.ScheduleOptions{AppKeys: []string{appKey}, Namespace: instanceID})
+	log.FailOnError(t, err, "Error scheduling task")
+	Dash.VerifyFatal(t, len(ctxs), 1, "Only one task should have started")
+
+	err = schedulerDriver.WaitForRunning(ctxs[0], defaultWaitTimeout, defaultWaitInterval)
+	log.FailOnError(t, err, "Error waiting for app to get to running state")
+	defer blowNamespaceForTest(t, instanceID, false)
+
+	//////////////////////
+	// Start migration //
+	////////////////////
+	migrationName := fmt.Sprintf("%s-%s", instanceID, uuid.New())
+	err = startAndValidateMigration(migrationName, cpName, []string{instanceID}, true)
+	log.FailOnError(t, err, "failed to validate migration")
+
 	////////////////////////////////
 	// Cleanup created resources //
 	//////////////////////////////
-	if err = cleanUpClusterPairTestResources(cpName, defaultAdminNamespace); err != nil {
-		Dash.Fatal("cluster pair resources cleanup failed, error=", err)
+	if err = stork.Instance().DeleteClusterPair(cpName, defaultAdminNamespace); err != nil && !k8s_errors.IsNotFound(err) {
+		Dash.Fatal("failed to delete cluster pair", err)
 	}
 
 	// If we are here then the test has passed
@@ -128,6 +155,7 @@ func testStorkCtlClusterPairNFSUnidirectional(t *testing.T) {
 		"nfs-server":      nfsSrvAddr,
 		"nfs-export-path": nfsSrvExpPath,
 		"nfs-sub-path":    fmt.Sprintf("test-%s", uuid.New()),
+		"mode":            "migration",
 	}
 
 	// Execute the storkctl command.
@@ -159,11 +187,36 @@ func testStorkCtlClusterPairNFSUnidirectional(t *testing.T) {
 		Dash.Fatal("cluster pair creation failed, expected success got=%s", actualOutput)
 	}
 
+	///////////////////////////////////////
+	// Deploy app for running migration //
+	//////////////////////////////////////
+	err = setSourceKubeConfig()
+	log.FailOnError(t, err, "Error resetting source config")
+	instanceID := "nfs-unidirectional-cp-test"
+	appKey := "mysql-1-pvc"
+
+	// Schedule mysql replicas.
+	ctxs, err := schedulerDriver.Schedule(instanceID,
+		scheduler.ScheduleOptions{AppKeys: []string{appKey}, Namespace: instanceID})
+	log.FailOnError(t, err, "Error scheduling task")
+	Dash.VerifyFatal(t, len(ctxs), 1, "Only one task should have started")
+
+	err = schedulerDriver.WaitForRunning(ctxs[0], defaultWaitTimeout, defaultWaitInterval)
+	log.FailOnError(t, err, "Error waiting for app to get to running state")
+	defer blowNamespaceForTest(t, instanceID, false)
+
+	//////////////////////
+	// Start migration //
+	////////////////////
+	migrationName := fmt.Sprintf("%s-%s", instanceID, uuid.New())
+	err = startAndValidateMigration(migrationName, cpName, []string{instanceID}, true)
+	log.FailOnError(t, err, "failed to validate migration")
+
 	////////////////////////////////
 	// Cleanup created resources //
 	//////////////////////////////
-	if err = cleanUpClusterPairTestResources(cpName, defaultAdminNamespace); err != nil {
-		Dash.Fatal("cluster pair resources cleanup failed, error=", err)
+	if err = stork.Instance().DeleteClusterPair(cpName, defaultAdminNamespace); err != nil && !k8s_errors.IsNotFound(err) {
+		Dash.Fatal("failed to delete cluster pair", err)
 	}
 
 	// If we are here then the test has passed
@@ -196,6 +249,7 @@ func testStorkCtlClusterPairNFSSecretCreation(t *testing.T) {
 		"provider":            "nfs",
 		"nfs-server":          nfsSrvAddr,
 		"nfs-export-path":     nfsSrvExpPath,
+		"mode":                "migration",
 		"nfs-sub-path":        fmt.Sprintf("test-%s", uuid.New()),
 		"nfs-timeout-seconds": "10",
 		"nfs-mount-opts":      "hard,timeo=10",
@@ -238,11 +292,38 @@ func testStorkCtlClusterPairNFSSecretCreation(t *testing.T) {
 	Dash.VerifyFatal(t, string(secretData.Data["nfsIOTimeoutInSecs"]), "10", "invalid timeout seconds value")
 	Dash.VerifyFatal(t, string(secretData.Data["mountOptions"]), "hard,timeo=10", "invalid mount option")
 
+	///////////////////////////////////////
+	// Deploy app for running migration //
+	//////////////////////////////////////
+	err = setSourceKubeConfig()
+	log.FailOnError(t, err, "Error resetting source config")
+	err = setSourceKubeConfig()
+	log.FailOnError(t, err, "Error resetting source config")
+	instanceID := "nfs-verify-secret"
+	appKey := "mysql-1-pvc"
+
+	// Schedule mysql replicas.
+	ctxs, err := schedulerDriver.Schedule(instanceID,
+		scheduler.ScheduleOptions{AppKeys: []string{appKey}, Namespace: instanceID})
+	log.FailOnError(t, err, "Error scheduling task")
+	Dash.VerifyFatal(t, len(ctxs), 1, "Only one task should have started")
+
+	err = schedulerDriver.WaitForRunning(ctxs[0], defaultWaitTimeout, defaultWaitInterval)
+	log.FailOnError(t, err, "Error waiting for app to get to running state")
+	defer blowNamespaceForTest(t, instanceID, false)
+
+	//////////////////////
+	// Start migration //
+	////////////////////
+	migrationName := fmt.Sprintf("%s-%s", instanceID, uuid.New())
+	err = startAndValidateMigration(migrationName, cpName, []string{instanceID}, true)
+	log.FailOnError(t, err, "failed to validate migration")
+
 	////////////////////////////////
 	// Cleanup created resources //
 	//////////////////////////////
-	if err = cleanUpClusterPairTestResources(cpName, defaultAdminNamespace); err != nil {
-		Dash.Fatal("cluster pair resources cleanup failed, error=", err)
+	if err = stork.Instance().DeleteClusterPair(cpName, defaultAdminNamespace); err != nil && !k8s_errors.IsNotFound(err) {
+		Dash.Fatal("failed to delete cluster pair", err)
 	}
 
 	// If we are here then the test has passed
@@ -250,29 +331,49 @@ func testStorkCtlClusterPairNFSSecretCreation(t *testing.T) {
 	log.InfoD("Test status at end of %s test: %s", t.Name(), testResult)
 }
 
-// cleanUpClusterPairTestResources cleans up the resources that get created during the cluster pair integration
-// test execution across both the source and destination cluster.
-func cleanUpClusterPairTestResources(name, namespace string) (err error) {
-	for i := 0; i < 2; i++ {
-		switch i {
-		case 0:
-			if err = setDestinationKubeConfig(); err != nil {
-				return err
-			}
-		case 1:
-			if err = setSourceKubeConfig(); err != nil {
-				return err
-			}
+// startAndValidateMigration starts the migration and validates the status if the migration was successful or not.
+func startAndValidateMigration(migrationName, cpName string, namespaces []string, expectSuccess bool) (err error) {
+	ns := strings.Join(namespaces, ",")
+	cmdFlags := map[string]string{
+		"namespace":   defaultAdminNamespace,
+		"clusterPair": cpName,
+		"namespaces":  ns,
+	}
+
+	factory := storkctl.NewFactory()
+	var outputBuffer bytes.Buffer
+	cmd := storkctl.NewCommand(factory, os.Stdin, &outputBuffer, os.Stderr)
+	cmdArgs := []string{"create", "migration", migrationName}
+	// add the custom args to the command
+	for key, value := range cmdFlags {
+		cmdArgs = append(cmdArgs, "--"+key)
+		if value != "" {
+			cmdArgs = append(cmdArgs, value)
 		}
-		if err = stork.Instance().DeleteClusterPair(name, namespace); err != nil && !k8s_errors.IsNotFound(err) {
-			return fmt.Errorf("failed to delete cluster pair,error=%s", err)
-		}
-		if err = stork.Instance().DeleteBackupLocation(name, namespace); err != nil && !k8s_errors.IsNotFound(err) {
-			return fmt.Errorf("failed to delete backuplocation,error=%s", err)
-		}
-		if err = core.Instance().DeleteSecret(name, namespace); err != nil && !k8s_errors.IsNotFound(err) {
-			return fmt.Errorf("failed to delete secret,error=%s", err)
-		}
+	}
+	cmd.SetArgs(cmdArgs)
+	// execute the command
+	log.InfoD("The storkctl command being executed is %v", cmdArgs)
+	if err = cmd.Execute(); err != nil {
+		err = fmt.Errorf("Storkctl execution failed: %v", err)
+		return err
+	}
+	// Get the captured output as a string
+	actualOutput := outputBuffer.String()
+	log.InfoD("Actual output is: %s", actualOutput)
+	expectedOutput := fmt.Sprintf("Migration %v created successfully\n", migrationName)
+	if actualOutput != expectedOutput {
+		return fmt.Errorf("Output mismatch,expected=%s,got=%s", expectedOutput, actualOutput)
+	}
+
+	// Check the status of the migration.
+	if err = stork.Instance().ValidateMigration(migrationName, defaultAdminNamespace, defaultWaitTimeout, defaultWaitInterval); err != nil && expectSuccess {
+		return fmt.Errorf("failed to validate migration %s,error=%v", migrationName, err)
+	}
+
+	// Cleanup the created migration.
+	if err = deleteAndwaitForMigrationDeletion(migrationName, defaultAdminNamespace, migrationRetryTimeout); err != nil {
+		return fmt.Errorf("failed to delete migration %s,error=%v", migrationName, err)
 	}
 	return
 }
